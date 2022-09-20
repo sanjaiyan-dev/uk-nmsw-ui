a2337415f6583bd97204af11c3369fe05693e57a
JenniKate Wallace <jennikate@gmail.com>
2022-09-20 17:09:54 +0100

    Add GDS header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import Header from './pages/layout/Header';
+import Header from './layout/Header';
 
 const App = () => {
   return (
     <>
       <Header />
-      <h1 className="govuk-heading-l">Basic setup</h1>
-      <p className="govuk-body">With a test paragraph so we can prove GovUK frontend style are working</p>
-      <button className="govuk-button" data-module="govuk-button">
-        And a test button
-      </button>
-      <br />
-      <span>and a span where we forget to add the class, but should default to GDS font</span>
+      <div className="govuk-width-container">
+        <main className="govuk-main-wrapper govuk-main-wrapper--auto-spacing" id="content" role="main">
+          <h1 className="govuk-heading-l">Basic setup</h1>
+          <p className="govuk-body">With a test paragraph so we can prove GovUK frontend style are working</p>
+          <button className="govuk-button" data-module="govuk-button">
+            And a test button
+          </button>
+          <br />
+          <span>and a span where we forget to add the class, but should default to GDS font</span>
+        </main>
+      </div>
     </>
   );
 };
