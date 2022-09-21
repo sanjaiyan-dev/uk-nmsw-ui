067b1eea3e11e9608099f867017b3c36ae73ce93
JenniKate Wallace <jennikate@gmail.com>
2022-09-21 16:19:33 +0100

    Add header, phase banner, and footer rendering as part of App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,24 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { SERVICE_NAME } from './constants/AppConstants.js';
 import App from './App.jsx';
-import Header from './layout/Header';
 
 describe('App tests', () => {
 
   // adding this test here on the assumption we form the page container in App, 
-  // if we move that to another file then should move this render heading test too
+  // if we move that to another file then should move this render heading, phase banner and footer tests too
   it('should render the heading on the page', async () => {
-    await waitFor(() => { render(<Header />); });
+    await waitFor(() => { render(<App />); });
     expect(screen.getByText('GOV.UK')).toBeInTheDocument();
     expect(screen.getByText(SERVICE_NAME)).toBeInTheDocument();
+  });
+
+  it('should render the phase banner on the page', async () => {
+    await waitFor(() => { render(<App />); });
+    const checkPhaseBannerText = screen.getByTestId('phaseBannerText');
+    const checkPhaseBannerLink = screen.getByText('feedback');
+    expect(checkPhaseBannerText).toHaveTextContent('This is a new service - your ');
+    expect(checkPhaseBannerLink).toBeInTheDocument();
+    expect(checkPhaseBannerText).toHaveTextContent(' will help us to improve it.');
   });
 
   it('should render the page with a h1', async () => {
@@ -18,5 +26,12 @@ describe('App tests', () => {
     const checkHeading = screen.getByText('Basic setup');
     expect(checkHeading).toBeInTheDocument();
     expect(checkHeading.outerHTML).toEqual('<h1 class="govuk-heading-l">Basic setup</h1>');
+  });
+
+  it('should render the footer on the page', async () => {
+    await waitFor(() => { render(<App />); });
+    const checkCrownCopyrightLogo = screen.getByText('© Crown copyright');
+    expect(checkCrownCopyrightLogo).toBeInTheDocument();
+    expect(checkCrownCopyrightLogo.outerHTML).toEqual('<a class="govuk-footer__link govuk-footer__copyright-logo" href="/">© Crown copyright</a>');
   });
 });
