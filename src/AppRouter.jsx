70539d945af8b89c855124dba43730c9bb73ccb1
JenniKate Wallace <jennikate@gmail.com>
2022-09-26 14:10:02 +0100

    Add protected route paths to dashboard and second page
    using a hardcoded user = 'something' or null to test for now

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,22 +1,30 @@
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
+import ProtectedRoute from './utils/ProtectedRoute';
 // URLs
 import {
+  DASHBOARD_URL,
   LANDING_URL,
   SIGN_IN_URL,
+  SECOND_PAGE_URL,
 } from './constants/AppUrlConstants';
 // Pages
+import Dashboard from './pages/Dashboard/Dashboard';
 import Landing from './pages/Landing/Landing';
 import SignIn from './pages/SignIn/SignIn';
+import SecondPage from './pages/TempPages/SecondPage';
 
 const AppRouter = () => {
+  const user = null;
+
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
       <Route path={LANDING_URL} element={<Landing />} />
       <Route path={SIGN_IN_URL} element={<SignIn />} />
+      <Route element={<ProtectedRoute user={user} />}>
+          <Route path={DASHBOARD_URL} element={<Dashboard />} />
+          <Route path={SECOND_PAGE_URL} element={<SecondPage />} />
+        </Route>
     </Routes>
   );
 };
