986e0b63c89244995e8fa903f2e85c8ac5d5b67d
JenniKate Wallace <jennikate@gmail.com>
2022-09-26 14:48:32 +0100

    Update logic for when to redirect into protected routes

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import { LANDING_URL } from '../constants/AppUrlConstants';
 
 const ProtectedRoute = ({ children, user }) => {
-  if (!user) {
+  /* States to redirect to login (regardless of the state of other key/values in user)
+     - no user.auth state
+     - no user.name state
+     - user.auth === false
+  */
+  if (!user.auth || !user.name || user.auth === false) {
     return <Navigate to={LANDING_URL} replace />; // replace replaces the current entry in the history stack, so back button will not go back to previous page
   }
 
@@ -13,9 +18,6 @@ const ProtectedRoute = ({ children, user }) => {
 export default ProtectedRoute;
 
 ProtectedRoute.propTypes = {
-  user: PropTypes.objectOf({
-    name: PropTypes.string,
-  }),
-  children: PropTypes.element // element indicates a React element should be rendered
-  // children: PropTypes.node // node means anything can be rendered
+  user: PropTypes.object, // there may or may not be a user prop, if there is it must be an object
+  children: PropTypes.element // there may or may not be a children prop, if there is it must be a React renderable element (if this breaks can change node - any renderable element)
 };
