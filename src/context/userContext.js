819dc3b81bd8c27f7d0e624fde3a65cd3ec00a1a
JenniKate Wallace <jennikate@gmail.com>
2022-09-26 15:04:53 +0100

    Remove temp data

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -5,13 +5,10 @@ import PropTypes from 'prop-types';
  * in future this can be extended to also include roles: [] so we can store the users
  * roles in the context and use for displaying pages actions etc.
 */
-const UserContext = createContext({
-  name: '',
-  auth: false
-});
+const UserContext = createContext({});
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ name: '', auth: false });
+  const [user, setUser] = useState({});
 
   // Login updates the user data with a name & roles
   // will replace this with the authentication  components later
