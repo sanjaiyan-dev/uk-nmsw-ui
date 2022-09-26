4dcf952108483a3f6480cf6eabc7d58e6ff2b7b1
JenniKate Wallace <jennikate@gmail.com>
2022-09-26 14:09:26 +0100

    Add protected route component

---
+++ src/utils/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { LANDING_URL } from '../constants/AppUrlConstants';
+
+const ProtectedRoute = ({ children, user }) => {
+  if (!user) {
+    return <Navigate to={LANDING_URL} replace />; // replace replaces the current entry in the history stack, so back button will not go back to previous page
+  }
+
+  return children ? children : <Outlet />;
+};
+
+export default ProtectedRoute;
+
+ProtectedRoute.propTypes = {
+  user: PropTypes.string,
+  children: PropTypes.element // element indicates a React element should be rendered
+  // children: PropTypes.node // node means anything can be rendered
+};
