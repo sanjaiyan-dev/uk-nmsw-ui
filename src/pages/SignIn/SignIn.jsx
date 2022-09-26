21bb38163fb70bb7d74c50ce73790c861d77f628
JenniKate Wallace <jennikate@gmail.com>
2022-09-26 14:30:38 +0100

    Add usercontext to sign in page and set signed in state

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,22 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/userContext';
+import { DASHBOARD_URL } from '../../constants/AppUrlConstants';
+
 const SignIn = () => {
+  const { login } = useContext(UserContext);
+  const navigate = useNavigate();
+  const tempHardCodedUser = { name: 'MockedUser' };
+
   return (
     <>
       <h1 className="govuk-heading-l">Sign in</h1>
+      <button
+        className="govuk-button" data-module="govuk-button"
+        onClick={async () => { await login({ ...tempHardCodedUser }); navigate({DASHBOARD_URL}); }}
+      >
+        Sign in
+      </button>
     </>
   );
 };
