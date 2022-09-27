f076102a9048e6d0b1cc6541e563336956f06992
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-09-27 10:03:51 +0100

    Wrap Footer test renders in MemoryRouter

--- src/layout/__tests__/Footer.test.jsx
+++ src/layout/__tests__/Footer.test.jsx
@@ -1,15 +1,16 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Footer from '../Footer';
 
 describe('Footer tests', () => {
 
   it('should render the footer with Crown Copyright text', async () => {
-    await waitFor(() => { render(<Footer />); });
+    await waitFor(() => { render(<MemoryRouter><Footer /></MemoryRouter>); });
     expect(screen.getByText('© Crown copyright')).toBeInTheDocument();
   });
 
   it('should render the Crown Copyright element with the class that contains the image', async () => {
-    await waitFor(() => { render(<Footer />); });
+    await waitFor(() => { render(<MemoryRouter><Footer /></MemoryRouter>); });
     const checkCrownCopyrightLogo = screen.getByText('© Crown copyright');
     expect(checkCrownCopyrightLogo).toBeInTheDocument();
     expect(checkCrownCopyrightLogo.outerHTML).toEqual('<a class="govuk-footer__link govuk-footer__copyright-logo" href="/">© Crown copyright</a>');
