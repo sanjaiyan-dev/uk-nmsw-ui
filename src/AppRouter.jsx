63f190587fed01fa093e7effbe3b01c3079dce89
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-09-27 11:36:44 +0100

    Add accessibility statement to AppRouter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,11 +4,14 @@ import {
 } from 'react-router-dom';
 // URLs
 import {
+  ACCESSIBILITY_URL,
   COOKIE_URL,
   LANDING_URL,
   SIGN_IN_URL,
 } from './constants/AppUrlConstants';
+
 // Pages
+import AccessibilityStatement from './pages/Footer/AccessibilityStatement';
 import CookiePolicy from './pages/Footer/CookiePolicy';
 import Landing from './pages/Landing/Landing';
 import SignIn from './pages/SignIn/SignIn';
@@ -20,6 +23,7 @@ const AppRouter = () => {
       <Route path={LANDING_URL} element={<Landing />} />
       <Route path={SIGN_IN_URL} element={<SignIn />} />
       <Route path={COOKIE_URL} element={<CookiePolicy />} />
+      <Route path={ACCESSIBILITY_URL} element={<AccessibilityStatement />} />
     </Routes>
   );
 };
