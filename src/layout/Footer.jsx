9f72e8b30629598ab01bcd5fa3fb0c2aebcb1185
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-09-28 14:12:17 +0100

    Put Privacy notice link in the Footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+
 import { ACCESSIBILITY_URL } from '../constants/AppUrlConstants';
 import { COOKIE_URL } from '../constants/AppUrlConstants';
+import { PRIVACY_URL } from '../constants/AppUrlConstants';
 
 const Footer = () => {
   return (
@@ -11,6 +13,11 @@ const Footer = () => {
             <h2 className="govuk-visually-hidden">Support links</h2>
             <ul className="govuk-footer__inline-list">
               <li className="govuk-footer__inline-list-item">
+                <Link className="govuk-footer__link" to={PRIVACY_URL}>
+                  Privacy
+                </Link>
+              </li>
+              <li className="govuk-footer__inline-list-item">
                 <Link className="govuk-footer__link" to={COOKIE_URL}>
                   Cookies
                 </Link>
@@ -20,11 +27,6 @@ const Footer = () => {
                   Accessibility
                 </Link>
               </li>
-              {/* <li className="govuk-footer__inline-list-item">
-                <a className="govuk-footer__link" href="#3">
-                  Item 3
-                </a>
-              </li> */}
             </ul>
           </div>
           <div className="govuk-footer__meta-item">
