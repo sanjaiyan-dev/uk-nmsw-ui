bc9f26acf65b1f0bb9459c5b6d7b37d676d66778
JenniKate Wallace <jennikate@gmail.com>
2022-09-28 10:10:24 +0100

    Add protected route test and comment re negative scenarios

--- src/utils/__tests__/ProtectedRoute.test.jsx
+++ src/utils/__tests__/ProtectedRoute.test.jsx
@@ -1,23 +1,31 @@
-import { UserContext } from '../../context/userContext';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { Dashboard } from '../../pages/Dashboard/Dashboard';
-import ProtectedRoute from '../ProtectedRoute';
+import Dashboard from '../../pages/Dashboard/Dashboard';
+import ProtectedRoute from '../ProtectedRoutes';
+
+/* Testing the 'not authorised' scenarios e.g. <ProtectedRoute user={{ name: 'bob', auth: false }}>
+ * tried rendering ProtectedRoute (without MemoryRouter) and it errors with '<Navigate>' must
+ * be inside a router
+ * However, when wrapping ProtectedRoute inside MemoryRouter or BrowserRouter
+ * in the negative scenario the test times out
+ * It seems to be something to do with <Navigate to=> being triggered and not completing
+ * the navigation function
+ * 
+ * Therefore we will test the negative routes with the Cypress E2E tests
+ */
 
 describe('Routing tests', () => {
-  it('should redirect to landing page if user not authorised', () => {
-
-   
-    const currentUser = { name: 'bob', auth: false };
-
+  it('should render a protected page if user is authorised', () => {
     render(
-      <UserContext.Provider value={{ currentUser }}>
-        <MemoryRouter initialEntries={['/sign-in']}>
-          <ProtectedRoute component={Dashboard} user={currentUser} />
-        </MemoryRouter>
-      </UserContext.Provider>
+      <MemoryRouter>
+        <ProtectedRoute user={{ name: 'bob', auth: true }}>
+          <Dashboard />
+        </ProtectedRoute>
+      </MemoryRouter>
     );
 
-    expect(screen.getByTestId('landing-h1')).toBeInTheDocument();
+    expect(screen.getByText('Dashboard')).toBeInTheDocument();
   });
+
 });
+
