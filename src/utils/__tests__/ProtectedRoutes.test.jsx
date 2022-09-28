fb74cbffae4e22d395323248b113d6b545d66ac8
JenniKate Wallace <jennikate@gmail.com>
2022-09-28 11:40:27 +0100

    Fixed negative protected routes test

--- src/utils/__tests__/ProtectedRoutes.test.jsx
+++ src/utils/__tests__/ProtectedRoutes.test.jsx
@@ -2,34 +2,37 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import ProtectedRoute from '../ProtectedRoutes';
 
-/* Testing the 'not authorised' scenarios e.g. <ProtectedRoute user={{ name: 'bob', auth: false }}>
- * tried rendering ProtectedRoute (without MemoryRouter) and it errors with '<Navigate>' must
- * be inside a router
- * However, when wrapping ProtectedRoute inside MemoryRouter or BrowserRouter
- * in the negative scenario the test times out
- * It seems to be something to do with <Navigate to=> being triggered and not completing
- * the navigation function in the test environment
- * 
- * Therefore we will test the negative routes with the Cypress E2E tests
- */
-
-
 describe('Routing tests', () => {
   const TestComponent = () => <div>test text</div>;
-  const isPermittedToView = true;
 
-  it('should fail', () => {
+  it('should render the page if the user is permitted to view', () => {
+    const isPermittedToView = true;
     render(
-      <MemoryRouter initialEntries={['/']}>
+      <MemoryRouter initialEntries={['/test']}>
         <Routes>
           <Route element={<ProtectedRoute isPermittedToView={isPermittedToView} />}>
-            <Route path="/" element={<TestComponent />} />
+            <Route path="/test" element={<TestComponent />} />
           </Route>
         </Routes>
       </MemoryRouter>
     );
 
     expect(screen.queryByText('test text')).toBeInTheDocument();
+  });
+
+  it('should NOT render the page if the user is NOT permitted to view', () => {
+    const isPermittedToView = false;
+    render(
+      <MemoryRouter initialEntries={['/test']}>
+        <Routes>
+          <Route element={<ProtectedRoute isPermittedToView={isPermittedToView} />}>
+            <Route path="/test" element={<TestComponent />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.queryByText('test text')).not.toBeInTheDocument();
   });
 
 });
