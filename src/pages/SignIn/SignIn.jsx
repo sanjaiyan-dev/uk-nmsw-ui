7e0e3b73c20c1cbfdb68eb4fc16881254462386b
JenniKate Wallace <jennikate@gmail.com>
2022-10-03 16:40:26 +0100

    Add a form to sign in page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,16 +1,45 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
+import { FIELD_EMAIL, FIELD_PASSWORD } from '../../constants/AppConstants';
 import { DASHBOARD_URL } from '../../constants/AppUrlConstants';
+import DisplayForm from '../../components/DisplayForm';
 
 const SignIn = (userDetails) => {
+  const tempHardCodedUser = Object.entries(userDetails).length > 0 ? userDetails.user : { name: 'MockedUser' };
   const { login } = useContext(UserContext);
   const navigate = useNavigate();
-  const tempHardCodedUser = Object.entries(userDetails).length > 0 ? userDetails.user : { name: 'MockedUser' };
+
+  // Form fields
+  const formFields = [
+    {
+      type: FIELD_EMAIL,
+      label: 'Email address',
+      hint: 'Enter the email address you used when you created your account',
+      fieldName: 'email',
+    },
+    {
+      type: FIELD_PASSWORD,
+      label: 'Password',
+      fieldName: 'password',
+    }
+  ];
+
+
+  const handleSubmit = (e, formData) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <>
       <h1 className="govuk-heading-l" data-testid="signin-h1">Sign in</h1>
+      <DisplayForm
+        fields={formFields}
+        handleSubmit={handleSubmit}
+      />
+
+      <hr />
       <button
         className="govuk-button"
         data-module="govuk-button"
@@ -21,7 +50,7 @@ const SignIn = (userDetails) => {
           navigate(DASHBOARD_URL);
         }}
       >
-        Sign in
+        Button to mock sign in to test nav
       </button>
     </>
   );
