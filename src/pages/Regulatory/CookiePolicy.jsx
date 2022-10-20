ab195197c072504c0d7c3e27849952eef3bc0c4f
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-10-20 15:18:11 +0100

    Add fields to CookiePolicy page

--- src/pages/Regulatory/CookiePolicy.jsx
+++ src/pages/Regulatory/CookiePolicy.jsx
@@ -1,7 +1,55 @@
+import DisplayForm from '../../components/DisplayForm';
+import { FIELD_RADIO } from '../../constants/AppConstants';
+
 const CookiePolicy = () => {
+
+  const formActions = {
+    submit: {
+      className: 'govuk-button',
+      dataModule: 'govuk-button',
+      dataTestid: 'save-button',
+      label: 'Save cookie settings',
+      type: 'button',
+    }
+  };
+  const formFields = [
+    {
+      type: FIELD_RADIO,
+      label: 'Do you want to accept analytics cookies?',
+      fieldName: 'cookieSettings',
+      className: 'govuk-radios',
+      grouped: true,
+      radioOptions: [
+        {
+          label: 'Yes',
+          name: 'cookieSettings',
+          id: 'yes',
+          value: 'yes'
+        },
+        {
+          label: 'No',
+          name: 'cookieSettings',
+          id: 'no',
+          value: 'no'
+        },
+      ]
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit');
+  };
+
   return (
     <>
       <h1 className="govuk-heading-l">Cookies</h1>
+      <h2 className="govuk-heading-l">Change your cookie settings</h2>
+      <DisplayForm
+        fields={formFields}
+        formActions={formActions}
+        handleSubmit={handleSubmit}
+      />
     </>
   );
 };
