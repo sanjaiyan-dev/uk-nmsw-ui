07c8aa50eee79ef85e3b956a4e4eb463d9a94828
JenniKate Wallace <jennikate@gmail.com>
2022-10-25 10:54:25 +0100

    Remove unnecessary steps from tests

--- src/pages/SignIn/SignIn.test.jsx
+++ src/pages/SignIn/SignIn.test.jsx
@@ -7,6 +7,8 @@ import SignIn from '../../pages/SignIn/SignIn';
 describe('Sign in tests', () => {
   const mockedLogin = jest.fn();
   const mockedLogout = jest.fn();
+  let scrollIntoViewMock = jest.fn();
+  window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
 
   function renderWithUserContext(userDetails) {
     return render(
@@ -48,36 +50,37 @@ describe('Sign in tests', () => {
 
   it('should NOT call the login function on sign in button click if there ARE errors', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.click(screen.getByTestId('submit-button'));
     expect(mockedLogin).not.toHaveBeenCalled();
   });
 
   it('should display the Error Summary if there are errors', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('There is a problem')).toBeInTheDocument();
   });
 
   it('should display the email required error if there is no email address', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('Enter your email address')).toBeInTheDocument();
   });
 
+  it('should scroll to email field and set focus on email input if user clicks on email required error link', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
+    await user.click(screen.getByTestId('submit-button'));
+    await user.click(screen.getByText('Enter your email address'));
+    expect(scrollIntoViewMock).toHaveBeenCalled();
+    expect(screen.getByRole('textbox', {name: /email/i})).toHaveFocus();
+  });
+
   it('should display the email invalid error if the email address has no @', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.type(screen.getByRole('textbox', {name: /email/i}), 'testemail');
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('Enter your email address in the correct format, like name@example.com')).toBeInTheDocument();
@@ -85,19 +88,25 @@ describe('Sign in tests', () => {
 
   it('should display the email invalid error if the email address has no .xx', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.type(screen.getByRole('textbox', {name: /email/i}), 'testemail@boo');
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('Enter your email address in the correct format, like name@example.com')).toBeInTheDocument();
   });
 
+  it('should scroll to email field and set focus on email input if user clicks on email invalid format error link', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
+    await user.type(screen.getByRole('textbox', {name: /email/i}), 'testemail@boo');
+    await user.click(screen.getByTestId('submit-button'));
+    await user.click(screen.getByText('Enter your email address in the correct format, like name@example.com'));
+    expect(scrollIntoViewMock).toHaveBeenCalled();
+    expect(screen.getByRole('textbox', {name: /email/i})).toHaveFocus();
+  });
+
   it('should NOT display the email errors if the email address is a valid format', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.type(screen.getByRole('textbox', {name: /email/i}), 'testemail@email.com');
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.queryByText('Enter your email address')).not.toBeInTheDocument();
@@ -106,18 +115,23 @@ describe('Sign in tests', () => {
 
   it('should display the password required error if there is no password', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('Enter your password')).toBeInTheDocument();
   });
 
+  it('should scroll to password field and set focus on password input if user clicks on password required error link', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
+    await user.click(screen.getByTestId('submit-button'));
+    await user.click(screen.getByText('Enter your password'));
+    expect(scrollIntoViewMock).toHaveBeenCalled();
+    expect(screen.getByTestId('password-passwordField')).toHaveFocus();
+  });
+
   it('should NOT display the password required error if a password is entered', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.type(screen.getByTestId('password-passwordField'), 'testpassword');
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.queryByText('Enter your password')).not.toBeInTheDocument();
@@ -125,28 +139,41 @@ describe('Sign in tests', () => {
 
   it('should display the sample field required error if there is no sample field text', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('Enter your sample field password')).toBeInTheDocument();
   });
 
+  it('should scroll to sample field and set focus on sample field input if user clicks on sample field required error link', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
+    await user.click(screen.getByTestId('submit-button'));
+    await user.click(screen.getByText('Enter your sample field password'));
+    expect(scrollIntoViewMock).toHaveBeenCalled();
+    expect(screen.getByTestId('sampleMinLengthTest-passwordField')).toHaveFocus();
+  });
+
   it('should display the sample field min length error if the text in the field is < 8 characters', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.type(screen.getByTestId('sampleMinLengthTest-passwordField'), 'one');
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.getByText('Sample field must be a minimum of 8 characters')).toBeInTheDocument();
   });
 
+  it('should scroll to sample field and set focus on sample field input if user clicks on sample field min length error link', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
+    await user.type(screen.getByTestId('sampleMinLengthTest-passwordField'), 'one');
+    await user.click(screen.getByTestId('submit-button'));
+    await user.click(screen.getByText('Sample field must be a minimum of 8 characters'));
+    expect(scrollIntoViewMock).toHaveBeenCalled();
+    expect(screen.getByTestId('sampleMinLengthTest-passwordField')).toHaveFocus();
+  });
+
   it('should NOT display any sample field errors if the text in the field is >= 8 characters', async () => {
     const user = userEvent.setup();
-    const userDetails = { name: 'MockedUser', auth: true };
-
-    renderWithUserContext(userDetails);
+    render(<MemoryRouter><SignIn /></MemoryRouter>);
     await user.type(screen.getByTestId('sampleMinLengthTest-passwordField'), '12345678');
     await user.click(screen.getByTestId('submit-button'));
     expect(screen.queryByText('Sample field must be a minimum of 8 characters')).not.toBeInTheDocument();
@@ -163,5 +190,4 @@ describe('Sign in tests', () => {
     await user.click(screen.getByTestId('submit-button'));
     expect(mockedLogin).toHaveBeenCalled();
   });
-
 });
