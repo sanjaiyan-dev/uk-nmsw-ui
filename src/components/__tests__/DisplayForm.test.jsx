761c89db07ff070854ffe5ab44f9f724526af727
JenniKate Wallace <jennikate@gmail.com>
2022-10-26 14:24:57 +0100

    Add tests for radio inputs

--- src/components/__tests__/DisplayForm.test.jsx
+++ src/components/__tests__/DisplayForm.test.jsx
@@ -1,5 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import DisplayForm from '../DisplayForm';
+import {
+  FIELD_EMAIL,
+  FIELD_PASSWORD,
+  FIELD_RADIO,
+  FIELD_TEXT,
+} from '../../constants/AppConstants';
 
 /*
  * These tests check that we can pass a variety of
@@ -42,34 +48,67 @@ describe('Display Form', () => {
   };
   const formTextInput = [
     {
-      type: 'text',
+      type: FIELD_TEXT,
       label: 'Text input',
       hint: 'This is a hint for a text input',
       fieldName: 'testField',
     }
   ];
+  const formRadioInput = [
+    {
+      type: FIELD_RADIO,
+      label: 'This is a radio button set',
+      fieldName: 'radioButtonSet',
+      className: 'govuk-radios',
+      grouped: true,
+      hint: 'radio hint',
+      radioOptions: [
+        {
+          label: 'Radio one',
+          name: 'radioButtonSet',
+          id: 'radioOne',
+          value: 'radioOne',
+          checked: true
+        },
+        {
+          label: 'Radio two',
+          name: 'radioButtonSet',
+          id: 'radioTwo',
+          value: 'radioTwo',
+          checked: false
+        },
+        {
+          label: 'Radio three',
+          name: 'radioButtonSet',
+          id: 'radioThree',
+          value: 'radioThree',
+          checked: false
+        },
+      ]
+    },
+  ];
   const formSpecialInputs = [
     {
-      type: 'text',
+      type: FIELD_TEXT,
       label: 'Text input',
       hint: 'This is a hint for a text input',
       fieldName: 'testField',
     },
     {
-      type: 'email',
+      type: FIELD_EMAIL,
       label: 'Email input',
       hint: 'This is a hint for an email input',
       fieldName: 'email',
     },
     {
-      type: 'password',
+      type: FIELD_PASSWORD,
       label: 'Password input',
       hint: 'This is a hint for a password input',
       fieldName: 'password',
     }
   ];
 
-  it('should render a submit and cancel button if both exist', async () => {
+  it('should render a submit and cancel button if both exist', () => {
     render(
       <DisplayForm
         formId="testForm"
@@ -82,7 +121,7 @@ describe('Display Form', () => {
     expect((screen.getByTestId('cancel-button')).outerHTML).toEqual('<button type="button" class="govuk-button govuk-button--secondary" data-module="govuk-button" data-testid="cancel-button">Cancel test button</button>');
   });
 
-  it('should render only a submit button if there is no cancel button', async () => {
+  it('should render only a submit button if there is no cancel button', () => {
     render(
       <DisplayForm
         formId="testForm"
@@ -95,7 +134,7 @@ describe('Display Form', () => {
     expect(screen.getAllByRole('button')).toHaveLength(1);
   });
 
-  it('should render a text input', async () => {
+  it('should render a text input', () => {
     render(
       <DisplayForm
         formId="testForm"
@@ -106,10 +145,29 @@ describe('Display Form', () => {
     );
     expect(screen.getByLabelText('Text input')).toBeInTheDocument();
     expect(screen.getByText('This is a hint for a text input').outerHTML).toEqual('<div id="testField-hint" class="govuk-hint">This is a hint for a text input</div>');
-    expect(screen.getByRole('textbox', {name: 'Text input'})).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: 'Text input' })).toBeInTheDocument();
   });
 
-  it('should render the special input types', async () => {
+  it('should render a radio button input', () => {
+    render(
+      <DisplayForm
+        formId="testForm"
+        fields={formRadioInput}
+        formActions={formActionsSubmitOnly}
+        handleSubmit={handleSubmit}
+      />
+    );
+    expect(screen.getByText('This is a radio button set')).toBeInTheDocument();
+    expect(screen.getByText('radio hint').outerHTML).toEqual('<div id="radioButtonSet-hint" class="govuk-hint">radio hint</div>');
+    expect(screen.getByLabelText('Radio one')).toBeInTheDocument();
+    expect(screen.getByLabelText('Radio two')).toBeInTheDocument();
+    expect(screen.getByLabelText('Radio three')).toBeInTheDocument();
+    expect(screen.getByRole('radio', { name: 'Radio one' })).toBeInTheDocument();
+    expect(screen.getByRole('radio', { name: 'Radio two' })).toBeInTheDocument();
+    expect(screen.getByRole('radio', { name: 'Radio three' })).toBeInTheDocument();
+  });
+
+  it('should render the special input types', () => {
     render(
       <DisplayForm
         formId="testForm"
@@ -121,11 +179,11 @@ describe('Display Form', () => {
     /* standard text field */
     expect(screen.getByLabelText('Text input')).toBeInTheDocument();
     expect(screen.getByText('This is a hint for a text input').outerHTML).toEqual('<div id="testField-hint" class="govuk-hint">This is a hint for a text input</div>');
-    expect(screen.getByRole('textbox', {name: 'Text input'})).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: 'Text input' })).toBeInTheDocument();
     /* email field */
     expect(screen.getByLabelText('Email input')).toBeInTheDocument();
     expect(screen.getByText('This is a hint for an email input').outerHTML).toEqual('<div id="email-hint" class="govuk-hint">This is a hint for an email input</div>');
-    expect(screen.getByRole('textbox', {name: 'Email input'})).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: 'Email input' })).toBeInTheDocument();
     /* password field: password inputs do not have a true textbox role so must be found via test id */
     expect(screen.getByLabelText('Password input')).toBeInTheDocument();
     expect(screen.getByText('This is a hint for a password input').outerHTML).toEqual('<div id="password-hint" class="govuk-hint">This is a hint for a password input</div>');
