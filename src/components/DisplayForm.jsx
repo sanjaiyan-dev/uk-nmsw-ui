bfdd9c95fed4b8d5e0e3bf564a2bdd5a6f40ee92
JenniKate Wallace <jennikate@gmail.com>
2022-10-26 16:02:17 +0100

    Only trigger setErrors if there are errors

--- src/components/DisplayForm.jsx
+++ src/components/DisplayForm.jsx
@@ -9,9 +9,11 @@ const DisplayForm = ({ errors, fields, formId, formActions, handleSubmit, setErr
   const [formData, setFormData] = useState({});
 
   const handleChange = (e) => {
-    // on change any error shown for that field should be cleared so find if field has an error & remove from error list
-    const filteredErrors = errors?.filter(errorField => errorField.name !== e.target.name);
-    setErrors(filteredErrors);
+    if (errors) {
+      // on change any error shown for that field should be cleared so find if field has an error & remove from error list
+      const filteredErrors = errors?.filter(errorField => errorField.name !== e.target.name);
+      setErrors(filteredErrors);
+    }
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
@@ -162,5 +164,5 @@ DisplayForm.propTypes = {
     })
   ),
   handleSubmit: PropTypes.func.isRequired,
-  setErrors: PropTypes.func,
+  setErrors: PropTypes.func
 };
