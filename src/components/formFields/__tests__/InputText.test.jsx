d8f40142798a58e27608fce97a1cb3263dbb8297
JenniKate Wallace <jennikate@gmail.com>
2022-11-10 13:20:43 +0000

    Extend input test coverage

--- src/components/formFields/__tests__/InputText.test.jsx
+++ src/components/formFields/__tests__/InputText.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { FIELD_EMAIL, FIELD_PASSWORD, FIELD_TEXT } from '../../../constants/AppConstants';
 import InputText from '../InputText';
 
 /*
@@ -44,4 +45,79 @@ describe('Text input field generation', () => {
     expect(screen.getByRole('textbox', { name: '' })).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: '' }).outerHTML).toEqual('<input class="govuk-input govuk-input--error" id="fullFieldName-input" data-testid="test-id" name="fullFieldName" type="text" autocomplete="email" aria-describedby="fullFieldName-hint" value="The full field value">');
   });
+
+  it('should render the input with type of text if text passed', () => {
+    render(
+      <InputText
+        fieldDetails={fieldDetailsAllProps}
+        handleChange={parentHandleChange}
+        type={FIELD_TEXT}
+      />
+    );
+    expect(screen.getByRole('textbox', { name: '' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: '' })).toHaveAttribute('type', 'text');
+  });
+
+  it('should render the input with type of email if email passed', () => {
+    render(
+      <InputText
+        fieldDetails={fieldDetailsAllProps}
+        handleChange={parentHandleChange}
+        type={FIELD_EMAIL}
+      />
+    );
+    expect(screen.getByRole('textbox', { name: '' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: '' })).toHaveAttribute('type', 'email');
+  });
+
+  it('should render the input with type of password if password passed', () => {
+    render(
+      <InputText
+        dataTestid='passwordField'
+        fieldDetails={fieldDetailsAllProps}
+        handleChange={parentHandleChange}
+        type={FIELD_PASSWORD}
+      />
+    );
+
+    expect(screen.getByTestId('passwordField')).toHaveAttribute('type', 'password');
+  });
+
+  it('should render the default value in the input text field when one is provided', () => {
+    render(
+      <InputText
+        fieldDetails={fieldDetailsAllProps}
+        handleChange={parentHandleChange}
+        type='text'
+      />
+    );
+    expect(screen.getByRole('textbox', { name: '' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: '' })).toHaveValue('The full field value');
+  });
+
+  it('should render the class as the error class if an error is passed', () => {
+    render(
+      <InputText
+        error='This field has an error'
+        fieldDetails={fieldDetailsAllProps}
+        handleChange={parentHandleChange}
+        type='text'
+      />
+    );
+    expect(screen.getByRole('textbox', { name: '' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: '' }).outerHTML).toEqual('<input class="govuk-input govuk-input--error" id="fullFieldName-input" name="fullFieldName" type="text" aria-describedby="fullFieldName-hint" value="The full field value">');
+  });
+
+  it('should NOT render the class as the error class if NO error is passed', () => {
+    render(
+      <InputText
+        fieldDetails={fieldDetailsAllProps}
+        handleChange={parentHandleChange}
+        type='text'
+      />
+    );
+    expect(screen.getByRole('textbox', { name: '' })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: '' }).outerHTML).toEqual('<input class="govuk-input" id="fullFieldName-input" name="fullFieldName" type="text" aria-describedby="fullFieldName-hint" value="The full field value">');
+  });
+
 });
