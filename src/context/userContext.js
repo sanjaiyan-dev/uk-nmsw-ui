16ec6e2d436398d56ec249dcdb3714cb15c41a7b
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-11-10 16:18:22 +0000

    Add auth functions

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -10,26 +10,43 @@ const UserContext = createContext({});
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
+  const storeToken = (token) => {
+    sessionStorage.setItem('token', token);
+  };
+
+  const retrieveToken = () => {
+    return sessionStorage.getItem('token');
+  };
+
+  const isAuthorized = () => {
+    return retrieveToken();
+  };
+
   // Login updates the user data with a name & roles
-  // will replace this with the authentication  components later
-  const login = ({ name }) => {
-    setUser(() => ({
-      name: name,
-      auth: true,
-    }));
+  // will replace this with the authentication components later
+  const signIn = ({ name }) => {
+    if (isAuthorized()) {
+      setUser(() => ({
+        name: name,
+        auth: true,
+      }));
+    }
   };
 
   // Logout updates the user data to default
   // will replace this with the authentication components later
-  const logout = () => {
+  const signOut = () => {
     setUser(() => ({
       name: '',
       auth: false,
     }));
+    sessionStorage.clear();
   };
 
+
+
   return (
-    <UserContext.Provider value={{ user, login, logout }}>
+    <UserContext.Provider value={{ user, signIn, signOut, storeToken, retrieveToken, isAuthorized }}>
       {children}
     </UserContext.Provider>
   );
