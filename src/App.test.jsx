0afa07f610c709edad192cbe36dfde93c6d8baf5
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2022-11-10 16:37:43 +0000

    Wrap renders in UserProvider

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,22 +2,23 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import { SERVICE_NAME } from './constants/AppConstants.js';
+import { UserProvider } from './context/userContext';
 import App from './App.jsx';
 
 describe('App tests', () => {
   it('should render the heading on the page', async () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
+    render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
     expect(screen.getByText('GOV.UK')).toBeInTheDocument();
     expect(screen.getByTestId('serviceName').textContent).toEqual(SERVICE_NAME);
   });
 
   it('should render the phase banner on the page', async () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
+    render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
     expect(screen.getByTestId('phaseBannerText')).toHaveTextContent('This is a new service - your feedback will help us to improve it.');
   });
 
   it('should render the footer on the page', async () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
+    render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
     expect(screen.getByText('© Crown copyright')).toBeInTheDocument();
     expect(screen.getByText('© Crown copyright').outerHTML).toEqual('<a class="govuk-footer__link govuk-footer__copyright-logo" target="_blank" rel="noreferrer noopener" href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/">© Crown copyright</a>');
     expect(screen.getByText('Cookies')).toBeInTheDocument();
@@ -29,7 +30,7 @@ describe('App tests', () => {
   });
 
   it('should render cookie banner when there is no cookiePreference cookie', async () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
+    render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
     const acceptButton = screen.getByRole('button', { name: 'Accept analytics cookies' });
     const rejectButton = screen.getByRole('button', { name: 'Reject analytics cookies' });
 
@@ -41,7 +42,7 @@ describe('App tests', () => {
 
   it('should hide the cookie confirmation banner once hide cookie message is clicked after user accepts or rejects cookies', async () => {
       const user = userEvent.setup();
-      render(<BrowserRouter><App /></BrowserRouter>);
+      render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
   
       const acceptButton = screen.getByRole('button', { name: 'Accept analytics cookies' });
       await user.click(acceptButton);
@@ -57,7 +58,7 @@ describe('App tests', () => {
 
   it('should not render cookie banner when cookiePreference is true', async () => {
     document.cookie = 'cookiePreference=true';
-    render(<BrowserRouter><App /></BrowserRouter>);
+    render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
     const acceptButton = screen.queryByRole('button', { name: 'Accept analytics cookies' });
     const rejectButton = screen.queryByRole('button', { name: 'Reject analytics cookies' });
 
@@ -69,7 +70,7 @@ describe('App tests', () => {
 
   it('should not render cookie banner when cookiePreference is false', async () => {
     document.cookie = 'cookiePreference=false';
-    render(<BrowserRouter><App /></BrowserRouter>);
+    render(<UserProvider><BrowserRouter><App /></BrowserRouter></UserProvider>);
     const acceptButton = screen.queryByRole('button', { name: 'Accept analytics cookies' });
     const rejectButton = screen.queryByRole('button', { name: 'Reject analytics cookies' });
 
--- src/layout/__tests__/Header.test.jsx
+++ src/layout/__tests__/Header.test.jsx
@@ -1,29 +1,30 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { SERVICE_NAME } from '../../constants/AppConstants';
+import { UserProvider } from '../../context/userContext';
 import Header from '../Header';
 
 describe('Header tests', () => {
 
-  it('should render the header with GovUK logo text', async () => {
-    await waitFor(() => { render(<MemoryRouter><Header /></MemoryRouter>); });
+  it('should render the header with GovUK logo text', () => {
+    render(<UserProvider><MemoryRouter><Header /></MemoryRouter></UserProvider>);
     expect(screen.getByText('GOV.UK')).toBeInTheDocument();
   });
 
   it('should render the service name text from the constants file', async () => {
-    await waitFor(() => { render(<MemoryRouter><Header /></MemoryRouter>); });
+    render(<UserProvider><MemoryRouter><Header /></MemoryRouter></UserProvider>);
     expect(screen.getByText(SERVICE_NAME)).toBeInTheDocument();
   });
 
   it('should render the expected Service name', async () => {
-    await waitFor(() => { render(<MemoryRouter><Header /></MemoryRouter>); });
+    render(<UserProvider><MemoryRouter><Header /></MemoryRouter></UserProvider>);
     expect(screen.getByText('National Maritime Single Window')).toBeInTheDocument();
   });
 
   it('should contain a skip link to allow screen reader users to easily skip to content element', async () => {
     const user = userEvent.setup();
-    await waitFor(() => { render(<MemoryRouter><Header /></MemoryRouter>); });
+    render(<UserProvider><MemoryRouter><Header /></MemoryRouter></UserProvider>);
     await user.keyboard('Tab');
 
     const checkSkipLink = screen.getByText('Skip to main content');
