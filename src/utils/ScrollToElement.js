0fbaaae167a786993f17efb0931514d23ef65a00
JenniKate Wallace <jennikate@gmail.com>
2022-11-22 16:50:51 +0000

    Update scroll to element to scroll to h1 for form errors

--- src/utils/ScrollToElement.js
+++ src/utils/ScrollToElement.js
@@ -2,3 +2,7 @@ export const scrollToElementId = (id) => {
     document.getElementById(id).scrollIntoView();
     document.activeElement.blur();
 };
+
+export const scrollToH1 = () => {
+  document.getElementsByTagName('html')[0].scrollIntoView();
+};
