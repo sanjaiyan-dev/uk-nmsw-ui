40ec18a5c23719efaf21263e620c65056b53d99b
JenniKate Wallace <jennikate@gmail.com>
2022-12-09 11:25:06 +0000

    Add page shells

---
+++ src/pages/Register/RegisterEmailAddress.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+const RegisterYourEmailAddress = () => {
+
+  return (
+    <>
+      <h1>Your EmailAddress page</h1>
+      <Link to="/create-account/your-password">next page</Link>
+    </>
+  );
+};
+
+export default RegisterYourEmailAddress;
