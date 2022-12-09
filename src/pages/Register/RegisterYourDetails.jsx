28c672cd59ae9288fb4ed532133b7641d48397b6
JenniKate Wallace <jennikate@gmail.com>
2022-12-09 13:08:02 +0000

    Add base form fields

--- src/pages/Register/RegisterYourDetails.jsx
+++ src/pages/Register/RegisterYourDetails.jsx
@@ -1,11 +1,95 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import {
+  FIELD_RADIO,
+  FIELD_TEXT,
+  VALIDATE_REQUIRED
+} from '../../constants/AppConstants';
+import { REGISTER_PASSWORD } from '../../constants/AppUrlConstants';
+import DisplayForm from '../../components/DisplayForm';
+
 const RegisterYourDetails = () => {
+  const navigate = useNavigate();
+
+  const formActions = {
+    submit: {
+      label: 'Continue',
+    },
+  };
+  const formFields = [
+    {
+      type: FIELD_TEXT,
+      fieldName: 'fullName',
+      label: 'Full name',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter your full name'
+        },
+      ]
+    },
+    {
+      type: FIELD_TEXT,
+      fieldName: 'companyName',
+      label: 'Your company name',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter your company name'
+        },
+      ]
+    },
+    {
+      type: FIELD_TEXT,
+      fieldName: 'country',
+      label: 'Country',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter country'
+        },
+      ]
+    },
+    {
+      type: FIELD_RADIO,
+      className: 'govuk-radios govuk-radios--inline',
+      fieldName: 'shippingAgent',
+      grouped: true,
+      label: 'Is your company a shipping agent?',
+      radioOptions: [
+        {
+          label: 'Yes',
+          name: 'shippingAgent',
+          value: 'yes',
+        },
+        {
+          label: 'No',
+          name: 'shippingAgent',
+          value: 'no',
+        },
+      ],
+    },
+  ];
+
+  const handleSubmit = async (e, formData) => {
+    // This will trigger a PATCH to /registration endpoint
+    // with { "email": "example@mail.com" }
+    console.log('submit', e, formData);
+    // and then take user to the next page
+    navigate(REGISTER_PASSWORD);
+  };
 
   return (
-    <>
-      <h1>Your details page</h1>
-      <Link to="/create-account/your-password">next page</Link>
-    </>
+    <div className="govuk-grid-row">
+      <div className="govuk-grid-column-two-thirds">
+        <DisplayForm
+          formId='formRegisterYourDetails'
+          fields={formFields}
+          formActions={formActions}
+          pageHeading='Your details'
+          handleSubmit={handleSubmit}
+        />
+      </div>
+    </div>
   );
 };
 
