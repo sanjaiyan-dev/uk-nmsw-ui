a8934609d885ae97907dab1ec08e811f8a578d04
JenniKate Wallace <jennikate@gmail.com>
2022-12-15 16:40:52 +0000

    Tidy up confirmation page and add tests

--- src/pages/Register/RegisterConfirmation.jsx
+++ src/pages/Register/RegisterConfirmation.jsx
@@ -4,19 +4,6 @@ import { SIGN_IN_URL } from '../../constants/AppUrlConstants';
 const RegisterConfirmation = () => {
   const { state } = useLocation();
 
-  if (!state || Object.entries(state).length < 1) {
-    return (
-      <div className="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" data-module="govuk-error-summary">
-        <h2 className="govuk-error-summary__title" id="error-summary-title">
-          There is a problem
-        </h2>
-        <div className="govuk-error-summary__body">
-          <p>Something went wrong, check your form submissions and try again</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-three-quarters">
@@ -24,9 +11,9 @@ const RegisterConfirmation = () => {
           <h1 className="govuk-panel__title">
             Account created
           </h1>
-          {state.companyName &&
+          {state?.companyName &&
             <div className="govuk-panel__body">
-              {`${state.companyName} has been setup.`}
+              {state?.companyName && `${state.companyName} has been setup.`}
             </div>
           }
         </div>
---
+++ src/pages/Register/__tests__/RegsiterConfirmation.test.jsx
@@ -0,0 +1,53 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { SIGN_IN_URL } from '../../../constants/AppUrlConstants';
+import RegisterConfirmation from '../RegisterConfirmation';
+
+const mockUseLocationState = { state: {} };
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useNavigate: () => mockedUseNavigate,
+  useLocation: jest.fn().mockImplementation(() => {
+    return mockUseLocationState;
+  })
+}));
+
+describe('Registration confirmation page tests', () => {
+
+  beforeEach(() => {
+    window.sessionStorage.clear();
+    mockUseLocationState.state = {};
+  });
+
+  it('should render h1', () => {
+    render(<MemoryRouter><RegisterConfirmation /></MemoryRouter>);
+    expect(screen.getByText('Account created')).toBeInTheDocument();
+  });
+
+  it('should render company name', () => {
+    mockUseLocationState.state = { companyName: 'My company' };
+    render(<MemoryRouter><RegisterConfirmation /></MemoryRouter>);
+    expect(screen.getByText('My company has been setup.')).toBeInTheDocument();
+  });
+
+  it('should not break if company name is lost from state', () => {
+    mockUseLocationState.state = {};
+    render(<MemoryRouter><RegisterConfirmation /></MemoryRouter>);
+    expect(screen.getByText('Account created')).toBeInTheDocument();
+    expect(screen.getByText('Sign in')).toBeInTheDocument();
+  });
+
+  it('should render a sign in link that links to sign in page', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><RegisterConfirmation /></MemoryRouter>);
+    expect(screen.getByText('Sign in')).toBeInTheDocument();
+    await user.click(screen.getByText('Sign in'));
+
+    await waitFor(() => {
+      expect(mockedUseNavigate).toHaveBeenCalledWith(SIGN_IN_URL,  {'preventScrollReset': undefined, 'relative': undefined, 'replace': false, 'state': undefined}); // params on Link generated links by default
+    });
+  });
+});
