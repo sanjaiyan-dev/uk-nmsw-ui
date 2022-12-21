5deea9d1099882df892cedadf3389345d43f5ee5
JenniKate Wallace <jennikate@gmail.com>
2022-12-21 13:56:24 +0000

    Change form width to one half rather than 3/4 of 2/3 to better match design

--- src/components/DisplayForm.jsx
+++ src/components/DisplayForm.jsx
@@ -188,59 +188,57 @@ const DisplayForm = ({ fields, formId, formActions, formType, pageHeading, handl
         </div>
       </div>
       <div className="govuk-grid-row">
-        <form id={formId} className="govuk-grid-column-two-thirds" autoComplete="off">
-          <div className="govuk-grid-column-three-quarters">
-            {
-              fieldsWithValues.map((field) => {
-                const error = errors?.find(errorField => errorField.name === field.fieldName);
-                return (
-                  <div
-                    key={field.fieldName}
-                    id={field.fieldName}
-                    ref={(node) => {
-                      const map = getFieldMap();
-                      if (node) {
-                        map.set(field.fieldName, node); // on mount adds the refs
-                      } else {
-                        map.delete(field.fieldName); // on unmount removes the refs
-                      }
-                    }}
-                  >
-                    {
-                      determineFieldType({
-                        allErrors: errors,  // allows us to add the error handling logic for conditional fields
-                        error: error?.message,
-                        fieldDetails: field,
-                        parentHandleChange: handleChange,
-                      })
+        <form id={formId} className="govuk-grid-column-one-half" autoComplete="off">
+          {
+            fieldsWithValues.map((field) => {
+              const error = errors?.find(errorField => errorField.name === field.fieldName);
+              return (
+                <div
+                  key={field.fieldName}
+                  id={field.fieldName}
+                  ref={(node) => {
+                    const map = getFieldMap();
+                    if (node) {
+                      map.set(field.fieldName, node); // on mount adds the refs
+                    } else {
+                      map.delete(field.fieldName); // on unmount removes the refs
                     }
-                  </div>
-                );
-              })
-            }
-            <div className="govuk-button-group">
-              <button
-                type='button'
-                className='govuk-button'
-                data-module='govuk-button'
-                data-testid='submit-button'
-                onClick={(e) => handleValidation(e, { formData })}
-              >
-                {formActions.submit.label}
-              </button>
-              {
-                formActions.cancel && <button
-                  type='button'
-                  className='govuk-button govuk-button--secondary'
-                  data-module='govuk-button'
-                  data-testid='cancel-button'
-                  onClick={() => handleCancel(formActions.cancel.redirectURL)}
+                  }}
                 >
-                  {formActions.cancel.label}
-                </button>
-              }
+                  {
+                    determineFieldType({
+                      allErrors: errors,  // allows us to add the error handling logic for conditional fields
+                      error: error?.message,
+                      fieldDetails: field,
+                      parentHandleChange: handleChange,
+                    })
+                  }
+                </div>
+              );
+            })
+          }
+          <div className="govuk-button-group">
+            <button
+              type='button'
+              className='govuk-button'
+              data-module='govuk-button'
+              data-testid='submit-button'
+              onClick={(e) => handleValidation(e, { formData })}
+            >
+              {formActions.submit.label}
+            </button>
+            {
+              formActions.cancel && <button
+                type='button'
+                className='govuk-button govuk-button--secondary'
+                data-module='govuk-button'
+                data-testid='cancel-button'
+                onClick={() => handleCancel(formActions.cancel.redirectURL)}
+              >
+                {formActions.cancel.label}
+              </button>
+            }
 
-            </div>
           </div>
         </form>
       </div>
