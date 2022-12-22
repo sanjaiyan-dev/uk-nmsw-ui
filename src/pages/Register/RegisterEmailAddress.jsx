09c5ea5bf9edccd21f00da9748fed3acceac3722
JenniKate Wallace <jennikate@gmail.com>
2022-12-22 13:06:35 +0000

    Load check your email page on /registration success

--- src/pages/Register/RegisterEmailAddress.jsx
+++ src/pages/Register/RegisterEmailAddress.jsx
@@ -7,7 +7,7 @@ import {
   VALIDATE_FIELD_MATCH,
   VALIDATE_REQUIRED
 } from '../../constants/AppConstants';
-import { ERROR_URL, REGISTER_EMAIL_URL, REGISTER_DETAILS_URL } from '../../constants/AppUrlConstants';
+import { ERROR_URL, REGISTER_EMAIL_URL, REGISTER_EMAIL_CHECK_URL } from '../../constants/AppUrlConstants';
 import usePostData from '../../hooks/usePostData';
 import DisplayForm from '../../components/DisplayForm';
 
@@ -77,11 +77,11 @@ const RegisterEmailAddress = () => {
         }
       });
       if (response && response.status === 200) {
-        navigate(REGISTER_DETAILS_URL);
+        navigate(REGISTER_EMAIL_CHECK_URL);
       } else {
         navigate(ERROR_URL, { state: { 
           message: response.message ? response.message : 'Something has gone wrong',
-          redirectURL: REGISTER_EMAIL_URL }});
+          redirectURL: REGISTER_EMAIL_CHECK_URL }});
       }
     } catch (err) {
       navigate(ERROR_URL, { state: { message: 'Something has gone wrong', redirectURL: REGISTER_EMAIL_URL }});
