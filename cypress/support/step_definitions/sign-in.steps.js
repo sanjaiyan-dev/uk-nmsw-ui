35e845e44f401e3f4012d87974708f76d38f88ea
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-01-04 08:07:49 +0000

    Add test to sign-in feature

---
+++ cypress/support/step_definitions/sign-in.steps.js
@@ -0,0 +1,37 @@
+import {Given, When, Then, Before} from '@badeball/cypress-cucumber-preprocessor';
+import EmailPage from '../../e2e/pages/registration/email.page.js';
+import SignInPage from '../../e2e/pages/sign-in.page.js';
+import {faker} from '@faker-js/faker';
+import LandingPage from '../../e2e/pages/landing.page';
+
+let email;
+let password;
+
+Before(() => {
+  email = faker.internet.email();
+  password = faker.internet.password();
+});
+
+Given('I am on the sign-in page', () => {
+  cy.visit('/');
+  LandingPage.clickStartNow();
+  SignInPage.checkSignInPage();
+});
+
+When('I click on the create one now link', () => {
+  SignInPage.clickCreateOneLink();
+});
+
+Then('I am taken to the create-account page', () => {
+  EmailPage.checkEmailPage();
+});
+
+When('I enter valid credentials and sign-in', () => {
+  SignInPage.EnterEmailAddress(email);
+  SignInPage.EnterPassword(password);
+  SignInPage.clickSignIn();
+});
+
+Then('I am taken to dashboard page', () => {
+  cy.get('h1').should('have.text', 'Dashboard');
+});
