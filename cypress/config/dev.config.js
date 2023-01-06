d96e7103873257a096e81b72b67bdcfe277f1578
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-01-06 14:11:19 +0000

    Add env config files

--- cypress/config/dev.config.js
+++ cypress/config/dev.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require('cypress')
-const defaultConfig = require('../../cypress.config')
+const {defineConfig} = require('cypress');
+const defaultConfig = require('../../cypress.config');
 
 module.exports = defineConfig({
       ...defaultConfig,
 
       env: {
-        "baseUrl": "https://nmsw-ui.dev.nmsw.homeoffice.gov.uk/",
-        "envName": "development",
-        "api_server": "https://nmsw-api.dev.nmsw.homeoffice.gov.uk//v1"
+        'baseUrl': 'https://nmsw-ui.dev.nmsw.homeoffice.gov.uk/',
+        'envName': 'development',
+        'api_server': 'https://nmsw-api.dev.nmsw.homeoffice.gov.uk//v1'
       }
     }
-)
+);
--- cypress/config/local.config.js
+++ cypress/config/local.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require('cypress')
-const defaultConfig = require('../../cypress.config')
+const {defineConfig} = require('cypress');
+const defaultConfig = require('../../cypress.config');
 
 module.exports = defineConfig({
       ...defaultConfig,
 
       env: {
-        "baseUrl": "http://localhost:3000/",
-        "envName": "development",
-        "api_server": "https://nmsw-api.dev.nmsw.homeoffice.gov.uk//v1"
+        'baseUrl': 'http://localhost:3000/',
+        'envName': 'development',
+        'api_server': 'https://nmsw-api.dev.nmsw.homeoffice.gov.uk//v1'
       }
     }
-)
+);
