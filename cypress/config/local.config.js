b3c9c8bc6af3d46b9590f3cc47e4271c452c5812
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-01-06 14:06:26 +0000

    Add configuration files for different test environments

---
+++ cypress/config/local.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress')
+const defaultConfig = require('../../cypress.config')
+
+module.exports = defineConfig({
+      ...defaultConfig,
+
+      env: {
+        "baseUrl": "http://localhost:3000/",
+        "envName": "development",
+        "api_server": "https://nmsw-api.dev.nmsw.homeoffice.gov.uk//v1"
+      }
+    }
+)
