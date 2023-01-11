2ff1bb69e81f32e3f76949bbeeb74e6606fa53c9
JenniKate Wallace <jennikate@gmail.com>
2023-01-11 17:06:42 +0000

    Add new endpoint constant

--- src/constants/AppAPIConstants.js
+++ src/constants/AppAPIConstants.js
@@ -2,6 +2,7 @@ import { apiUrl } from './Config';
 
 // Create account/register pages
 export const REGISTER_ACCOUNT_ENDPOINT = `${apiUrl}/registration`;
+export const REGISTER_CHECK_TOKEN_ENDPOINT = `${apiUrl}/check-token`;
 
 // Responses
 export const AXIOS_ERROR = 'Network Error';
