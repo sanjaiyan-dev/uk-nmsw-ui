683488cee9974375d6de783ff36a6071d415e328
JenniKate Wallace <jennikate@gmail.com>
2023-01-12 11:57:37 +0000

    Remove unneeded return

--- src/utils/setAnalyticCookie.js
+++ src/utils/setAnalyticCookie.js
@@ -1,12 +1,10 @@
 const setAnalyticCookie = (cookiePreference) => {
-  if (cookiePreference === true ) {
+  if (cookiePreference === true) {
     document.cookie = 'cookiePreference=true';
     console.log('GA ON');
   } else if (cookiePreference === false) {
     document.cookie = 'cookiePreference=false';
     console.log('GA OFF');
-  } else {
-    return;
   }
 };
 
