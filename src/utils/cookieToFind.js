98178aee7b94ca32d0ffc5cbb863b87d34fcc538
JenniKate Wallace <jennikate@gmail.com>
2023-01-12 12:02:06 +0000

    Fix spacing, equals, and remove redundant else block

--- src/utils/cookieToFind.js
+++ src/utils/cookieToFind.js
@@ -2,13 +2,12 @@ const findCookiePreference = (cookieName) => {
   const cookieArray = document.cookie.split(';');
 
   for (let i = 0; i < cookieArray.length; i++) {
-    let cookiePair = cookieArray[i].split('=');
-    if (cookieName == cookiePair[0].trim()) {
+    const cookiePair = cookieArray[i].split('=');
+    if (cookieName === cookiePair[0].trim()) {
       if (cookiePair[1] === 'true') {
         return true;
-      } else {
-        return false;
       }
+      return false;
     }
   }
   return null;
