f5631a47b1c70506b6343ca69a601f299738901a
JenniKate Wallace <jennikate@gmail.com>
2023-01-16 11:03:01 +0000

    Update message page to take a button and no back link

--- src/pages/Message/GenericMessage.jsx
+++ src/pages/Message/GenericMessage.jsx
@@ -1,16 +1,29 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { SERVICE_NAME } from '../../constants/AppConstants';
 import { LANDING_URL } from '../../constants/AppUrlConstants';
 
 const GenericMessage = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
   document.title = state?.title || SERVICE_NAME;
   return (
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-two-thirds">
         <h1 className="govuk-heading-xl">{state?.title}</h1>
         {state?.message && <p className="govuk-body">{state?.message}</p>}
-        <Link to={state?.redirectURL || LANDING_URL}>Click here to continue</Link>
+        {state?.pageContent.buttonLabel && (
+          <button
+            className="govuk-button"
+            data-module="govuk-button"
+            type="button"
+            onClick={() => { navigate(state?.pageContent.buttonNavigateTo, state?.pageContent.buttonState); }}
+          >
+            {state?.pageContent.buttonLabel}
+          </button>
+        )}
+        {!state.pageContent.buttonLabel && (
+          <Link to={state?.redirectURL || LANDING_URL}>Click here to continue</Link>
+        )}
       </div>
     </div>
   );
