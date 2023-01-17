69844b0f544a267af4259328bfff1155d8a8c892
JenniKate Wallace <jennikate@gmail.com>
2023-01-17 11:29:35 +0000

    Add test for content render

--- src/pages/Register/RegisterEmailResend.jsx
+++ src/pages/Register/RegisterEmailResend.jsx
@@ -1,14 +1,61 @@
 import { useLocation } from 'react-router-dom';
+import {
+  DISPLAY_DETAILS,
+  FIELD_EMAIL,
+  SINGLE_PAGE_FORM,
+  VALIDATE_EMAIL_ADDRESS,
+} from '../../constants/AppConstants';
+import DisplayForm from '../../components/DisplayForm';
+
+const SupportingText = () => (
+  <p className="govuk-body">Emails sometimes take a few minutes to arrive. If you didn&apos;t receive the link, you can request a new one.</p>
+);
 
 const RegisterEmailResend = () => {
   const { state } = useLocation();
   document.title = 'Resend verification email';
 
+  const formActions = {
+    submit: {
+      className: 'govuk-button',
+      dataModule: 'govuk-button',
+      dataTestid: 'submit-button',
+      label: 'Send confirmation email',
+      type: 'button',
+    },
+  };
+  const formFields = [
+    {
+      type: FIELD_EMAIL,
+      displayType: DISPLAY_DETAILS,
+      fieldName: 'emailAddress',
+      label: 'Email address',
+      linkText: 'Change where the email was sent',
+      validation: [
+        {
+          type: VALIDATE_EMAIL_ADDRESS,
+          message: 'Enter an email address in the correct format, like name@example.com',
+        },
+      ],
+    },
+  ];
+
+  const handleSubmit = (formData) => {
+    console.log('state', state);
+    console.log('formData', formData);
+  };
+
   return (
-    <>
-      <h1 className="govuk-heading-xl">Resend page</h1>
-      <strong className="govuk-label--s">{state?.dataToSubmit.emailAddress}</strong>
-    </>
+    <DisplayForm
+      formId="formSecondPage"
+      fields={formFields}
+      formActions={formActions}
+      formType={SINGLE_PAGE_FORM}
+      pageHeading="Request a new verification link"
+      handleSubmit={handleSubmit}
+    >
+      <SupportingText />
+    </DisplayForm>
   );
 };
 
--- src/pages/Register/__tests__/RegisterEmailResend.test.jsx
+++ src/pages/Register/__tests__/RegisterEmailResend.test.jsx
@@ -18,12 +18,9 @@ describe('Resend registration email verification email', () => {
   it('should render page with content', () => {
     mockUseLocationState = { state: { dataToSubmit: { emailAddress: 'testemail@email.com' } } };
     render(<MemoryRouter><RegisterEmailResend /></MemoryRouter>);
-    expect(screen.getByText('Resend page')).toBeInTheDocument();
-  });
-
-  it('should show the email address from state in the page', () => {
-    mockUseLocationState = { state: { dataToSubmit: { emailAddress: 'testemail@email.com' } } };
-    render(<MemoryRouter><RegisterEmailResend /></MemoryRouter>);
-    expect(screen.getByText('testemail@email.com')).toBeInTheDocument();
+    expect(screen.getByText('Request a new verification link')).toBeInTheDocument();
+    expect(screen.getByText('Emails sometimes take a few minutes to arrive. If you didn\'t receive the link, you can request a new one.')).toBeInTheDocument();
+    expect(screen.getByTestId('details-component').outerHTML).toEqual('<details class="govuk-details" data-module="govuk-details" data-testid="details-component"><summary class="govuk-details__summary"><span class="govuk-details__summary-text">Change where the email was sent</span></summary><div class="govuk-details__text"><label class="govuk-label" for="emailAddress-input">Email address</label><div id="emailAddress-hint" class="govuk-hint"></div><p id="emailAddress-error" class="govuk-error-message"><span class="govuk-visually-hidden">Error:</span> </p><input class="govuk-input" id="emailAddress-input" name="emailAddress" type="email" autocomplete="email" value=""></div></details>');
+    expect(screen.getByRole('button', { name: 'Send confirmation email' })).toBeInTheDocument('');
   });
 });
