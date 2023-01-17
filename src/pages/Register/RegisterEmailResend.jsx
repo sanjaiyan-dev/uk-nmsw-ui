54a2ebb42981191276389c0d141c728486359fe1
JenniKate Wallace <jennikate@gmail.com>
2023-01-17 13:53:13 +0000

    Add structure for POST to /resend-email-verification logic

--- src/pages/Register/RegisterEmailResend.jsx
+++ src/pages/Register/RegisterEmailResend.jsx
@@ -1,4 +1,10 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import {
+  REGISTER_RESEND_VERIFICATION_EMAIL_ENDPOINT,
+  USER_ALREADY_VERIFIED,
+  USER_NOT_REGISTERED,
+} from '../../constants/AppAPIConstants';
 import {
   DISPLAY_DETAILS,
   FIELD_EMAIL,
@@ -6,6 +12,10 @@ import {
   VALIDATE_EMAIL_ADDRESS,
   VALIDATE_REQUIRED,
 } from '../../constants/AppConstants';
+import {
+  MESSAGE_URL,
+  REGISTER_EMAIL_URL,
+} from '../../constants/AppUrlConstants';
 import DisplayForm from '../../components/DisplayForm';
 
 const SupportingText = () => (
@@ -14,6 +24,7 @@ const SupportingText = () => (
 
 const RegisterEmailResend = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
   document.title = 'Resend verification email';
 
   const formActions = {
@@ -46,8 +57,28 @@ const RegisterEmailResend = () => {
     },
   ];
 
-  const handleSubmit = (formData) => {
-    console.log('formData', formData?.formData.emailAddress);
+  const handleSubmit = async (formData) => {
+    try {
+      const controller = new AbortController();
+      const response = await axios.post(REGISTER_RESEND_VERIFICATION_EMAIL_ENDPOINT, {
+        email: formData.formData.emailAddress,
+      }, {
+        signal: controller.signal,
+      });
+
+      if (response.status === 204) {
+        console.log(response);
+      }
+    } catch (err) {
+      if (err?.response?.data?.message === USER_ALREADY_VERIFIED) {
+        console.log('already verified direct to sign in');
+      } else if (err?.response?.data?.message === USER_NOT_REGISTERED) {
+        console.log('not registered we need to POST to register user');
+      } else {
+        // 500 errors will fall into this bucket
+        navigate(MESSAGE_URL, { state: { title: 'Something has gone wrong', message: err.response?.data?.message, redirectURL: REGISTER_EMAIL_URL } });
+      }
+    }
   };
 
   return (
