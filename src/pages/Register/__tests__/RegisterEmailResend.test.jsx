448e2acc06e9e8c3d183749e9883c3ea01b12315
JenniKate Wallace <jennikate@gmail.com>
2023-01-17 12:03:49 +0000

    Update test to cover render with and without state

--- src/pages/Register/__tests__/RegisterEmailResend.test.jsx
+++ src/pages/Register/__tests__/RegisterEmailResend.test.jsx
@@ -15,12 +15,20 @@ describe('Resend registration email verification email', () => {
     mockUseLocationState = {};
   });
 
-  it('should render page with content', () => {
-    mockUseLocationState = { state: { dataToSubmit: { emailAddress: 'testemail@email.com' } } };
+  it('should render page with content when there is no state', () => {
     render(<MemoryRouter><RegisterEmailResend /></MemoryRouter>);
     expect(screen.getByText('Request a new verification link')).toBeInTheDocument();
     expect(screen.getByText('Emails sometimes take a few minutes to arrive. If you didn\'t receive the link, you can request a new one.')).toBeInTheDocument();
     expect(screen.getByTestId('details-component').outerHTML).toEqual('<details class="govuk-details" data-module="govuk-details" data-testid="details-component"><summary class="govuk-details__summary"><span class="govuk-details__summary-text">Change where the email was sent</span></summary><div class="govuk-details__text"><label class="govuk-label" for="emailAddress-input">Email address</label><div id="emailAddress-hint" class="govuk-hint"></div><p id="emailAddress-error" class="govuk-error-message"><span class="govuk-visually-hidden">Error:</span> </p><input class="govuk-input" id="emailAddress-input" name="emailAddress" type="email" autocomplete="email" value=""></div></details>');
+    expect(screen.getByRole('button', { name: 'Send confirmation email' })).toBeInTheDocument('');
+  });
+
+  it('should render page with content when email address is in state', () => {
+    mockUseLocationState = { state: { dataToSubmit: { emailAddress: 'testemail@email.com' } } };
+    render(<MemoryRouter><RegisterEmailResend /></MemoryRouter>);
+    expect(screen.getByText('Request a new verification link')).toBeInTheDocument();
+    expect(screen.getByText('Emails sometimes take a few minutes to arrive. If you didn\'t receive the link, you can request a new one.')).toBeInTheDocument();
+    expect(screen.getByTestId('details-component').outerHTML).toEqual('<details class="govuk-details" data-module="govuk-details" data-testid="details-component"><summary class="govuk-details__summary"><span class="govuk-details__summary-text">Change where the email was sent</span></summary><div class="govuk-details__text"><label class="govuk-label" for="emailAddress-input">Email address</label><div id="emailAddress-hint" class="govuk-hint"></div><p id="emailAddress-error" class="govuk-error-message"><span class="govuk-visually-hidden">Error:</span> </p><input class="govuk-input" id="emailAddress-input" name="emailAddress" type="email" autocomplete="email" value="testemail@email.com"></div></details>');
     expect(screen.getByRole('button', { name: 'Send confirmation email' })).toBeInTheDocument('');
   });
 });
