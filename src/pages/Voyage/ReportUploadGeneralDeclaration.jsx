1d37cf6000ae1755bad419af8b8b0a2bd0ad137a
JenniKate Wallace <jennikate@gmail.com>
2023-01-19 15:00:01 +0000

    Add general declaration upload page component

---
+++ src/pages/Voyage/ReportUploadGeneralDeclaration.jsx
@@ -0,0 +1,9 @@
+const ReportUploadGeneralDeclaration = () => {
+  document.title = 'Upload the General Declaration (FAL 1)';
+
+  return (
+    <h1 className="govuk-heading-xl">Upload the General Declaration (FAL 1)</h1>
+  );
+};
+
+export default ReportUploadGeneralDeclaration;
