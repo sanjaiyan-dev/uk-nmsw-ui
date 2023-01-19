6e2714bb3b075353127d02eb20725e8b4b0337a3
JenniKate Wallace <jennikate@gmail.com>
2023-01-19 15:47:51 +0000

    Add content, download link, and button to upload declaratio page

--- src/pages/Voyage/VoyageGeneralDeclaration.jsx
+++ src/pages/Voyage/VoyageGeneralDeclaration.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { DownloadFile } from '../../utils/DownloadFile';
 import { VOYAGE_GENERAL_DECLARATION_CONFIRMATION_URL } from '../../constants/AppUrlConstants';
 
 const ReportUploadGeneralDeclaration = () => {
@@ -7,13 +8,24 @@ const ReportUploadGeneralDeclaration = () => {
 
   return (
     <>
-      <h1 className="govuk-heading-xl">Upload the General Declaration (FAL 1)</h1>
-      <button
-        type="button"
-        onClick={() => navigate(VOYAGE_GENERAL_DECLARATION_CONFIRMATION_URL, { state: { fileType: 'General Declaration' } })}
-      >
-        click
-      </button>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h1 className="govuk-heading-xl">Upload the General Declaration (FAL 1)</h1>
+          <p className="govuk-body">You must use the new excel spreadsheet version of the <button className="govuk-button--text" type="button" onClick={() => DownloadFile('/assets/files/NMSW-FAL-1.xlsx', 'FAL1.xlsx')}>FAL 1 general declaration</button>.</p>
+        </div>
+      </div>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <button
+            type="button"
+            className="govuk-button"
+            data-module="govuk-button"
+            onClick={() => navigate(VOYAGE_GENERAL_DECLARATION_CONFIRMATION_URL, { state: { fileType: 'General Declaration' } })}
+          >
+            Save and continue
+          </button>
+        </div>
+      </div>
     </>
   );
 };
