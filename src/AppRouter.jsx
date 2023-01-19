334eb6aef08209b947ac03c72a7eb7aba6dc6ded
JenniKate Wallace <jennikate@gmail.com>
2023-01-19 11:45:28 +0000

    Remove second page from App Router and reorder imports/routes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import useUserIsPermitted from './hooks/useUserIsPermitted';
 import ProtectedRoutes from './utils/ProtectedRoutes';
 import ScrollToTopOnNewPage from './utils/ScrollToTopOnNewPage';
-
 import { SERVICE_NAME } from './constants/AppConstants';
 
 // URLs
@@ -26,17 +25,15 @@ import {
   REGISTER_DETAILS_URL,
   REGISTER_PASSWORD_URL,
   SIGN_IN_URL,
-  SECOND_PAGE_URL,
-  YOUR_VOYAGES_URL,
   TEMPLATE_PAGE_URL,
+  YOUR_VOYAGES_URL,
 } from './constants/AppUrlConstants';
 
-// Regulatory pages
-import AccessibilityStatement from './pages/Regulatory/AccessibilityStatement';
-import CookiePolicy from './pages/Regulatory/CookiePolicy';
-import Landing from './pages/Landing/Landing';
-import PrivacyNotice from './pages/Regulatory/PrivacyNotice';
+// Error/Message pages
+import GenericMessage from './pages/Message/GenericMessage';
 // Register/Sign in pages
+import AccountAlreadyActive from './pages/Message/AccountAlreadyActive';
+import VerificationLinkFailed from './pages/Message/VerificationLinkFailed';
 import RegisterConfirmation from './pages/Register/RegisterConfirmation';
 import RegisterEmailAddress from './pages/Register/RegisterEmailAddress';
 import RegisterEmailCheck from './pages/Register/RegisterEmailCheck';
@@ -45,18 +42,18 @@ import RegisterEmailVerified from './pages/Register/RegisterEmailVerified';
 import RegisterYourDetails from './pages/Register/RegisterYourDetails';
 import RegisterYourPassword from './pages/Register/RegisterYourPassword';
 import SignIn from './pages/SignIn/SignIn';
-// Error/Message pages
-import GenericMessage from './pages/Message/GenericMessage';
-import AccountAlreadyActive from './pages/Message/AccountAlreadyActive';
-import VerificationLinkFailed from './pages/Message/VerificationLinkFailed';
-// Other pages (could be protected or not)
-import FormConfirmationPage from './pages/Message/FormConfirmationPage';
-// Protected pages
+// Regulatory pages
+import AccessibilityStatement from './pages/Regulatory/AccessibilityStatement';
+import CookiePolicy from './pages/Regulatory/CookiePolicy';
+import Landing from './pages/Landing/Landing';
+import PrivacyNotice from './pages/Regulatory/PrivacyNotice';
+
+// PROTECTED PAGES
+// Voyage pages
 import YourVoyages from './pages/NavPages/YourVoyages';
 import ErrorsCrewUpload from './pages/Voyage/ErrorsCrewUpload';
-
-// Temp Pages
-import SecondPage from './pages/TempPages/SecondPage';
+// Other pages
+import FormConfirmationPage from './pages/Message/FormConfirmationPage';
 import Templates from './pages/NavPages/Templates';
 
 const AppRouter = ({ setIsCookieBannerShown }) => {
@@ -67,11 +64,10 @@ const AppRouter = ({ setIsCookieBannerShown }) => {
     <ScrollToTopOnNewPage>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path={ACCESSIBILITY_URL} element={<AccessibilityStatement />} />
-        <Route path={COOKIE_URL} element={<CookiePolicy setIsCookieBannerShown={setIsCookieBannerShown} />} />
-        <Route path={LANDING_URL} element={<Landing />} />
-        <Route path={PRIVACY_URL} element={<PrivacyNotice />} />
+        <Route path={MESSAGE_URL} element={<GenericMessage />} />
 
+        <Route path={ERROR_ACCOUNT_ALREADY_ACTIVE_URL} element={<AccountAlreadyActive />} />
+        <Route path={ERROR_VERIFICATION_FAILED_URL} element={<VerificationLinkFailed />} />
         <Route path={REGISTER_CONFIRMATION_URL} element={<RegisterConfirmation />} />
         <Route path={REGISTER_ACCOUNT_URL} element={<RegisterEmailAddress />} />
         <Route path={REGISTER_EMAIL_URL} element={<RegisterEmailAddress />} />
@@ -82,16 +78,16 @@ const AppRouter = ({ setIsCookieBannerShown }) => {
         <Route path={REGISTER_PASSWORD_URL} element={<RegisterYourPassword />} />
         <Route path={SIGN_IN_URL} element={<SignIn />} />
 
-        <Route path={MESSAGE_URL} element={<GenericMessage />} />
-        <Route path={ERROR_ACCOUNT_ALREADY_ACTIVE_URL} element={<AccountAlreadyActive />} />
-        <Route path={ERROR_VERIFICATION_FAILED_URL} element={<VerificationLinkFailed />} />
-
-        <Route path={FORM_CONFIRMATION_URL} element={<FormConfirmationPage />} />
+        <Route path={ACCESSIBILITY_URL} element={<AccessibilityStatement />} />
+        <Route path={COOKIE_URL} element={<CookiePolicy setIsCookieBannerShown={setIsCookieBannerShown} />} />
+        <Route path={LANDING_URL} element={<Landing />} />
+        <Route path={PRIVACY_URL} element={<PrivacyNotice />} />
 
         <Route element={<ProtectedRoutes isPermittedToView={isPermittedToView} />}>
-          <Route path={YOUR_VOYAGES_URL} element={<YourVoyages />} />
-          <Route path={SECOND_PAGE_URL} element={<SecondPage />} />
+          <Route path={FORM_CONFIRMATION_URL} element={<FormConfirmationPage />} />
           <Route path={TEMPLATE_PAGE_URL} element={<Templates />} />
+
+          <Route path={YOUR_VOYAGES_URL} element={<YourVoyages />} />
           <Route path={ERROR_CREW_DETAILS_UPLOAD_URL} element={<ErrorsCrewUpload />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
