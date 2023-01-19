4afa2d1c74c0df9d8ef3ebf4f2b1d366bdacee44
JenniKate Wallace <jennikate@gmail.com>
2023-01-19 15:26:33 +0000

    Add success notification to confirmation page

--- src/pages/Voyage/FileUploadConfirmation.jsx
+++ src/pages/Voyage/FileUploadConfirmation.jsx
@@ -2,12 +2,32 @@ import { useLocation } from 'react-router-dom';
 
 const FileUploadConfirmation = () => {
   const { state } = useLocation();
-  document.title = state.title;
-
-  console.log(state);
+  document.title = 'No errors found';
 
   return (
-    <h1 className="govuk-heading-xl">No errors found</h1>
+    <>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h1 className="govuk-heading-xl">No errors found</h1>
+        </div>
+      </div>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <div className="govuk-notification-banner govuk-notification-banner--success" role="alert" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
+            <div className="govuk-notification-banner__header">
+              <h2 className="govuk-notification-banner__title" id="govuk-notification-banner-title">
+                Success
+              </h2>
+            </div>
+            <div className="govuk-notification-banner__content">
+              <h3 className="govuk-notification-banner__heading">
+                {`${state.fileType ? state.fileType : ''} uploaded`}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Voyage/VoyageGeneralDeclaration.jsx
+++ src/pages/Voyage/VoyageGeneralDeclaration.jsx
@@ -10,7 +10,7 @@ const ReportUploadGeneralDeclaration = () => {
       <h1 className="govuk-heading-xl">Upload the General Declaration (FAL 1)</h1>
       <button
         type="button"
-        onClick={() => navigate(VOYAGE_GENERAL_DECLARATION_CONFIRMATION_URL, { state: { title: 'Heres a title' } })}
+        onClick={() => navigate(VOYAGE_GENERAL_DECLARATION_CONFIRMATION_URL, { state: { fileType: 'General Declaration' } })}
       >
         click
       </button>
