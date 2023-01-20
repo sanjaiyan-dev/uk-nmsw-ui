42bb6ea67c840e02bcd7aa659e7292ea2bb209c4
JenniKate Wallace <jennikate@gmail.com>
2023-01-20 15:40:22 +0000

    Fix test describe statements

--- src/pages/Voyage/__tests__/VoyagePassengerUpload.test.jsx
+++ src/pages/Voyage/__tests__/VoyagePassengerUpload.test.jsx
@@ -12,7 +12,7 @@ jest.mock('react-router', () => ({
   useLocation: jest.fn().mockImplementation(() => mockUseLocationState),
 }));
 
-describe('File upload success confirmation page', () => {
+describe('Voyage passenger upload page', () => {
   beforeEach(() => {
     window.sessionStorage.clear();
     mockUseLocationState.state = {};
