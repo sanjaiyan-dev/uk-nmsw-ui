f2fd7e93ee5f3d788b35920d98d281016b4cca3c
JenniKate Wallace <jennikate@gmail.com>
2023-01-20 11:27:39 +0000

    Fix route and add skeleton crew details page

--- src/pages/Voyage/VoyageCrew.jsx
+++ src/pages/Voyage/VoyageCrew.jsx
@@ -1,7 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import { VOYAGE_CREW_CONFIRMATION_URL } from '../../constants/AppUrlConstants';
+
 const VoyageCrew = () => {
-  console.log('crew page');
+  const navigate = useNavigate();
+  document.title = 'Upload the crew details including supernumeraries (FAL 5)';
+
   return (
-    <h1>Crew</h1>
+    <>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h1 className="govuk-heading-xl">Upload the crew details including supernumeraries (FAL 5)</h1>
+        </div>
+      </div>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          <button
+            type="button"
+            className="govuk-button"
+            data-module="govuk-button"
+            onClick={() => navigate(VOYAGE_CREW_CONFIRMATION_URL, { state: { fileType: 'Crew details' } })}
+          >
+            Save and continue
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
