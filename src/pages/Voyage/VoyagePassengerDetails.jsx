caf0f9297a0f1c46b057ab92d79bd6cee1141b77
JenniKate Wallace <jennikate@gmail.com>
2023-01-20 11:55:39 +0000

    Create passenger routes and skeleton pages

---
+++ src/pages/Voyage/VoyagePassengerDetails.jsx
@@ -0,0 +1,16 @@
+const VoyagePassengerDetails = () => {
+  document.title = 'Is the ship carrying any passengers?';
+
+  return (
+    <>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-full">
+          <h1 className="govuk-heading-xl">Is the ship carrying any passengers?</h1>
+        </div>
+      </div>
+      <p>form goes here</p>
+    </>
+  );
+};
+
+export default VoyagePassengerDetails;
