3399d28df2cc7588d25bb678fcaebe1317f2f9b7
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-01-30 15:12:46 +0000

    Update names and file paths

--- src/pages/NavPages/Templates.jsx
+++ src/pages/NavPages/Templates.jsx
@@ -10,13 +10,13 @@ const Templates = () => {
         <p className="govuk-body">Use the templates provided:</p>
         <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-7">
           <li>
-            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/NMSW-FAL-1.xlsx', 'FAL1.xlsx')}>General Declaration (FAL 1) template (xls)</button>
+            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/General declaration FAL 1.xlsx', 'General declaration FAL 1.xlsx')}>General Declaration (FAL 1) template (xlsx, 31kb)</button>
           </li>
           <li>
-            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/NMSW-FAL-5-and-6.xlsx', 'FAL5.xlsx')}>Crew details including supernumeraries (FAL 5) template (xls)</button>
+            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/Crew details including supernumeraries FAL 5.xlsx', 'Crew details including supernumeraries FAL 5.xlsx')}>Crew details including supernumeraries (FAL 5) template (xlsx, 118kb)</button>
           </li>
           <li>
-            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/NMSW-FAL-5-and-6.xlsx', 'FAL6.xlsx')}>Passenger details (FAL 6) template (xls)</button>
+            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/Passenger details FAL 6.xlsx', 'Passenger details FAL 6.xlsx')}>Passenger details (FAL 6) template (xlsx, 90kb)</button>
           </li>
         </ul>
         <details className="govuk-details" data-module="govuk-details">
