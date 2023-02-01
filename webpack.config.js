ee8ddf06e20ab64b34112c7b28e32681f27fa4be
JenniKate Wallace <jennikate@gmail.com>
2023-02-01 10:39:57 +0000

    Split nodemodules into smaller chunks

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,27 @@ module.exports = {
     filename: '[name].[contenthash].js',
     publicPath: '/',
   },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '/assets': path.resolve(__dirname, 'node_modules/govuk-frontend/govuk/assets'),
