0871c444f5d205e57bb8d671d6a6df0f05fc5d42
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-02-01 09:41:27 +0000

    Add sign out tests

--- src/layout/__tests__/Nav.test.jsx
+++ src/layout/__tests__/Nav.test.jsx
@@ -1,15 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { SERVICE_NAME } from '../../constants/AppConstants';
 import App from '../../App';
 import Nav from '../Nav';
+import { SIGN_OUT_ENDPOINT } from '../../constants/AppAPIConstants';
+import { SIGN_IN_URL } from '../../constants/AppUrlConstants';
 
 let mockedUserIsPermitted = false;
 jest.mock('../../hooks/useUserIsPermitted', () => jest.fn(() => (mockedUserIsPermitted)));
 
+const mockedUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUseNavigate,
+}));
+
 describe('Navigation within header tests', () => {
+  const mockAxios = new MockAdapter(axios);
+
   beforeEach(() => {
+    mockAxios.reset();
     window.sessionStorage.clear();
   });
 
@@ -111,5 +124,34 @@ describe('Navigation within header tests', () => {
     render(<MemoryRouter><App /></MemoryRouter>);
     await user.click(screen.getByText('Templates'));
     expect(window.sessionStorage.getItem('formData')).toStrictEqual(null);
+  });
+
+  it('should clear token from session and redirect to sign in page on successful sign out', async () => {
+    window.sessionStorage.setItem('token', '123');
+    mockedUserIsPermitted = true;
+    const user = userEvent.setup();
+    mockAxios
+      .onPost(SIGN_OUT_ENDPOINT)
+      .reply(200);
+
+    render(<MemoryRouter><App /></MemoryRouter>);
+    await user.click(screen.getByText('Sign out'));
+    expect(window.sessionStorage.getItem('token')).toStrictEqual(null);
+    expect(mockedUseNavigate).toHaveBeenCalledWith(SIGN_IN_URL);
+  });
+
+  // Error case will usually be when a user clicks sign out but token is already expired
+  it('should clear token from session and redirect to sign in page on sign out', async () => {
+    window.sessionStorage.setItem('token', '123');
+    mockedUserIsPermitted = true;
+    const user = userEvent.setup();
+    mockAxios
+      .onPost(SIGN_OUT_ENDPOINT)
+      .reply(401);
+
+    render(<MemoryRouter><App /></MemoryRouter>);
+    await user.click(screen.getByText('Sign out'));
+    expect(window.sessionStorage.getItem('token')).toStrictEqual(null);
+    expect(mockedUseNavigate).toHaveBeenCalledWith(SIGN_IN_URL);
   });
 });
