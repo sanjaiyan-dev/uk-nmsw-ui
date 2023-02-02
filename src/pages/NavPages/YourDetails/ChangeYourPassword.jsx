5596681ac6bb66f05e500589d36cdeea2bb17baa
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-02-02 13:57:59 +0000

    Create ChangeYourPassword page (h1 only)

---
+++ src/pages/NavPages/YourDetails/ChangeYourPassword.jsx
@@ -0,0 +1,7 @@
+import { CHANGE_YOUR_PASSWORD_PAGE_NAME } from '../../../constants/AppUrlConstants';
+
+const ChangeYourPassword = () => (
+  <h1 className="govuk-heading-xl">{CHANGE_YOUR_PASSWORD_PAGE_NAME}</h1>
+);
+
+export default ChangeYourPassword;
