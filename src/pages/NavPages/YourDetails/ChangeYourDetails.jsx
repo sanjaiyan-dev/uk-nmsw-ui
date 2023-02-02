5a7970fab42e7746474a93b8fdb71f077e9d9c85
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-02-02 13:57:40 +0000

    Create ChangeYourDetails page (h1 only)

---
+++ src/pages/NavPages/YourDetails/ChangeYourDetails.jsx
@@ -0,0 +1,7 @@
+import { CHANGE_YOUR_DETAILS_PAGE_NAME } from '../../../constants/AppUrlConstants';
+
+const ChangeYourDetails = () => (
+  <h1 className="govuk-heading-xl">{CHANGE_YOUR_DETAILS_PAGE_NAME}</h1>
+);
+
+export default ChangeYourDetails;
