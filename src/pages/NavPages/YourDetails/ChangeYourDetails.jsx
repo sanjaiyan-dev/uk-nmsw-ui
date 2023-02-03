29ba11237e7662530f87eeef6f627676413d8869
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-02-03 11:06:21 +0000

    Create an edit details skeleton form

--- src/pages/NavPages/YourDetails/ChangeYourDetails.jsx
+++ src/pages/NavPages/YourDetails/ChangeYourDetails.jsx
@@ -1,7 +1,109 @@
-import { CHANGE_YOUR_DETAILS_PAGE_NAME } from '../../../constants/AppUrlConstants';
+import { useNavigate } from 'react-router-dom';
+import DisplayForm from '../../../components/DisplayForm';
+import {
+  FIELD_TEXT, SINGLE_PAGE_FORM, VALIDATE_MAX_LENGTH, VALIDATE_REQUIRED,
+} from '../../../constants/AppConstants';
+import {
+  CHANGE_YOUR_DETAILS_PAGE_NAME, GENERIC_CONFIRMATION_URL, YOUR_DETAILS_PAGE_NAME, YOUR_DETAILS_PAGE_URL,
+} from '../../../constants/AppUrlConstants';
 
-const ChangeYourDetails = () => (
-  <h1 className="govuk-heading-xl">{CHANGE_YOUR_DETAILS_PAGE_NAME}</h1>
-);
+const ChangeYourDetails = () => {
+  const navigate = useNavigate();
+
+  const formActions = {
+    submit: {
+      label: 'Continue',
+    },
+  };
+  const formFields = [
+    {
+      type: FIELD_TEXT,
+      label: 'Full name',
+      fieldName: 'fullName',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter your full name',
+        },
+      ],
+    },
+    {
+      type: FIELD_TEXT,
+      label: 'Your company name',
+      fieldName: 'companyName',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter your company name',
+        },
+      ],
+    },
+    {
+      type: FIELD_TEXT,
+      label: 'International dialling code',
+      fieldName: 'dialingCode',
+      hint: 'For example, 44 for UK',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter your country code',
+        },
+      ],
+    },
+    {
+      type: FIELD_TEXT,
+      label: 'Telephone number',
+      fieldName: 'telephoneNumber',
+      hint: 'For example, 7123123123',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter your telephone number',
+        },
+      ],
+    },
+    {
+      type: FIELD_TEXT,
+      fieldName: 'country',
+      label: 'Country',
+      validation: [
+        {
+          type: VALIDATE_REQUIRED,
+          message: 'Enter country',
+        },
+        {
+          type: VALIDATE_MAX_LENGTH,
+          message: 'Enter 3 digit country code',
+          condition: 3,
+        },
+      ],
+    },
+  ];
+
+  const handleSubmit = async () => {
+    navigate(
+      GENERIC_CONFIRMATION_URL,
+      {
+        state: {
+          pageTitle: 'Change your details confirmation',
+          nextPageLink: YOUR_DETAILS_PAGE_URL,
+          nextPageName: YOUR_DETAILS_PAGE_NAME,
+          confirmationMessage: 'Your details have been saved',
+        },
+      },
+    );
+  };
+
+  return (
+    <DisplayForm
+      formId="changeYourDetails"
+      fields={formFields}
+      formActions={formActions}
+      formType={SINGLE_PAGE_FORM}
+      pageHeading={CHANGE_YOUR_DETAILS_PAGE_NAME}
+      handleSubmit={handleSubmit}
+    />
+  );
+};
 
 export default ChangeYourDetails;
