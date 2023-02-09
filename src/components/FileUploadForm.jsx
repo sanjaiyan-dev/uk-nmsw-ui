7b4c0a71df30b7e61acae88502647cc33dd3fe98
JenniKate Wallace <jennikate@gmail.com>
2023-02-09 12:01:40 +0000

    Render file upload component, with error block available

---
+++ src/components/FileUploadForm.jsx
@@ -0,0 +1,107 @@
+import { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const FileUploadForm = ({
+  formId,
+  pageHeading,
+  submitButtonLabel,
+  children,
+}) => {
+  const fileInputRef = useRef(null);
+  const fileUploadId = 'fileUploadInput';
+  const [error, setError] = useState();
+  const [selectedFile, setSelectedFile] = useState({});
+
+  const scrollToErrorField = () => {
+    fileInputRef.current.scrollIntoView({ block: 'start' });
+  };
+
+  const handleFileSelected = (e) => {
+    setSelectedFile({
+      file: e.target.files[0],
+    });
+    setError(); // clear any errors
+  };
+
+  const handleUpload = async () => {
+    console.log('upload clicked for', selectedFile);
+  };
+
+  return (
+    <>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-two-thirds">
+          {error && (
+            <div className="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" data-module="govuk-error-summary">
+              <h2 className="govuk-error-summary__title" id="error-summary-title">
+                There is a problem
+              </h2>
+              <div className="govuk-error-summary__body">
+                <ul className="govuk-list govuk-error-summary__list">
+                  <li>
+                    <button
+                      className="govuk-button--text"
+                      type="button"
+                      onClick={(e) => {
+                        scrollToErrorField(e, error.id);
+                      }}
+                    >
+                      {error?.message}
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="govuk-grid-row">
+        {pageHeading && <h1 className="govuk-heading-xl govuk-grid-column-full">{pageHeading}</h1>}
+      </div>
+      <div className="govuk-grid-row">
+        <div className="govuk-grid-column-three-quarters below-h1">
+          {children}
+        </div>
+      </div>
+      <div className="govuk-grid-row">
+        <form id={formId} className="govuk-grid-column-full" autoComplete="off">
+          <div className={error ? 'govuk-form-group govuk-form-group--error' : 'govuk-form-group'}>
+            <label className="govuk-label" htmlFor={fileUploadId} ref={fileInputRef}>
+              Upload a file
+            </label>
+            <p id={`${fileUploadId}-error`} className="govuk-error-message">
+              <span className="govuk-visually-hidden">Error:</span> {error?.message}
+            </p>
+            <input
+              className="govuk-file-upload"
+              data-testid={fileUploadId}
+              id={fileUploadId}
+              name={fileUploadId}
+              type="file"
+              onChange={handleFileSelected}
+            />
+          </div>
+          <div className="govuk-button-group">
+            <button
+              type="button"
+              className="govuk-button"
+              data-module="govuk-button"
+              onClick={handleUpload}
+            >
+              {submitButtonLabel}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FileUploadForm;
+
+FileUploadForm.propTypes = {
+  children: PropTypes.node, // allows any renderable object
+  formId: PropTypes.string.isRequired,
+  pageHeading: PropTypes.string.isRequired,
+  submitButtonLabel: PropTypes.string.isRequired,
+};
