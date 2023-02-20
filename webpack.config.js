4b45b2faf4b47f9e7ac11a21c529331831d9dd49
JenniKate Wallace <jennikate@gmail.com>
2023-02-20 15:34:08 +0000

    Add the args into webpack so they can be used to set Dev Mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,95 +6,96 @@ const path = require('path');
 const webpack = require('webpack');
 const env = require('./exportProcessEnv');
 
-const devMode = process.env.NODE_ENV !== 'production';
+module.exports = (argv) => {
+  const devMode = argv.mode !== 'production';
 
-module.exports = {
-  entry: ['./src/index.js', './src/assets/main.scss'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    publicPath: '/',
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+  return {
+    entry: ['./src/index.js', './src/assets/main.scss'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      publicPath: '/',
     },
-  },
-  performance: {
-    maxAssetSize: 512000,
-    maxEntrypointSize: 1024000,
-  },
-  resolve: {
-    alias: {
-      '/assets': path.resolve(__dirname, 'node_modules/govuk-frontend/govuk/assets'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        resolve: {
-          extensions: ['.js', '.jsx'],
-        },
-        use: ['babel-loader'],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
       },
-      {
-        test: /\.(sa|sc|c)ss$/, // styles files
-        use: [
-          { loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader }, // Creates `style` nodes from JS strings
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                quietDeps: true, // https://frontend.design-system.service.gov.uk/importing-css-assets-and-javascript/#silence-deprecation-warnings-from-dependencies-in-dart-sass
+    },
+    performance: {
+      maxAssetSize: 512000,
+      maxEntrypointSize: 1024000,
+    },
+    resolve: {
+      alias: {
+        '/assets': path.resolve(__dirname, 'node_modules/govuk-frontend/govuk/assets'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          resolve: {
+            extensions: ['.js', '.jsx'],
+          },
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(sa|sc|c)ss$/, // styles files
+          use: [
+            { loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader }, // Creates `style` nodes from JS strings
+            { loader: 'css-loader' },
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  quietDeps: true, // https://frontend.design-system.service.gov.uk/importing-css-assets-and-javascript/#silence-deprecation-warnings-from-dependencies-in-dart-sass
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        type: 'asset/inline',
-      },
-    ],
-  },
-  plugins: [
-    new Dotenv({ systemvars: true }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-      favicon: './src/assets/images/favicon.ico',
-      api: env.NMSW_DATA_API_BASE_URL,
-    }),
-    new CopyPlugin(
-      {
-        patterns: [
-          { from: 'node_modules/govuk-frontend/govuk/all.js', to: 'javascript/all.js' },
-          { from: 'node_modules/govuk-frontend/govuk/assets', to: 'assets' },
-          { from: 'src/assets/images', to: 'assets/images' },
-          { from: 'src/assets/css', to: 'assets/css' },
-          { from: 'src/assets/files', to: 'assets/files' },
-        ],
-      },
-    ),
-    // This allows to pass env vars on runtime, see /nginx/run.sh and Dockerfile
-    new webpack.EnvironmentPlugin([
-      'NMSW_DATA_API_BASE_URL',
-      'GA_TOKEN',
-    ]),
-  ].concat(devMode ? [] : [new MiniCssExtractPlugin({
-    filename: '[name]-[hash].css',
-    chunkFilename: '[id]-[hash].css',
-  })]),
-  devServer: {
-    historyApiFallback: true,
-    hot: false,
-    liveReload: true,
-    static: {
-      directory: path.join(__dirname, 'dist'),
+          ],
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+          type: 'asset/inline',
+        },
+      ],
     },
-    port: 3000,
-  },
-
+    plugins: [
+      new Dotenv({ systemvars: true }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+        favicon: './src/assets/images/favicon.ico',
+        api: env.NMSW_DATA_API_BASE_URL,
+      }),
+      new CopyPlugin(
+        {
+          patterns: [
+            { from: 'node_modules/govuk-frontend/govuk/all.js', to: 'javascript/all.js' },
+            { from: 'node_modules/govuk-frontend/govuk/assets', to: 'assets' },
+            { from: 'src/assets/images', to: 'assets/images' },
+            { from: 'src/assets/css', to: 'assets/css' },
+            { from: 'src/assets/files', to: 'assets/files' },
+          ],
+        },
+      ),
+      // This allows to pass env vars on runtime, see /nginx/run.sh and Dockerfile
+      new webpack.EnvironmentPlugin([
+        'NMSW_DATA_API_BASE_URL',
+        'GA_TOKEN',
+      ]),
+    ].concat(devMode ? [] : [new MiniCssExtractPlugin({
+      filename: '[name]-[hash].css',
+      chunkFilename: '[id]-[hash].css',
+    })]),
+    devServer: {
+      historyApiFallback: true,
+      hot: false,
+      liveReload: true,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      port: 3000,
+    },
+  };
 };
