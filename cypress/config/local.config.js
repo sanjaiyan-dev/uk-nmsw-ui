8189f3ebee91eb8ade5de48c28f73ecaba59bdfb
Jennifer Wallace <jennikate@gmail.com>
2023-03-21 15:26:06 +0000

    Recreate cypress config from v0.10.0

--- cypress/config/local.config.js
+++ cypress/config/local.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
         'baseUrl': 'http://localhost:3000',
         'envName': 'development',
         'api_server': 'http://localhost:5000/v1',
-        'inboxId': 'b6f7c995-d7b0-48e7-a1b6-9264b9598b37'
+        'inboxId': 'e5fc776c-a811-4cc5-9392-019f3872938b'
       }
     }
 );
--- cypress/config/staging.config.js
+++ cypress/config/staging.config.js
@@ -5,9 +5,9 @@ module.exports = defineConfig({
       ...defaultConfig,
 
       env: {
-        'baseUrl': 'https://nmsw-ui.dev.nmsw.homeoffice.gov.uk',
-        'envName': 'development',
-        'api_server': 'https://nmsw-api.dev.nmsw.homeoffice.gov.uk/v1',
+        'baseUrl': 'https://nmsw-ui.staging.nmsw.homeoffice.gov.uk',
+        'envName': 'staging',
+        'api_server': 'https://nmsw-api.staging.nmsw.homeoffice.gov.uk/v1',
         'inboxId': 'e5fc776c-a811-4cc5-9392-019f3872938b'
       }
     }
