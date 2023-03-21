23573c1317269d53b8b21d264ab7a699ad2b17c2
Jennifer Wallace <jennikate@gmail.com>
2023-03-21 15:34:17 +0000

    Recreate cypress steps from v0.10.0

---
+++ cypress/support/step_definitions/fal5-upload-crew-details.steps.js
@@ -0,0 +1,10 @@
+import {Then, When} from "@badeball/cypress-cucumber-preprocessor";
+import FileUploadPage from "../../e2e/pages/file-upload.page";
+
+When('I click crew details link', () => {
+  cy.contains('Crew details').click();
+});
+
+Then('I am taken to upload-crew-details page', () => {
+  FileUploadPage.verifyUploadCrewDetailsPage();
+});
