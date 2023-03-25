a8591b5f10e341e3ac05ef877e676f58f6e50b1a
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-03-25 09:32:20 +0000

    Change mailslurp inboxid

--- cypress/config/staging.config.js
+++ cypress/config/staging.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
         'baseUrl': 'https://nmsw-ui.staging.nmsw.homeoffice.gov.uk',
         'envName': 'staging',
         'api_server': 'https://nmsw-api.staging.nmsw.homeoffice.gov.uk/v1',
-        'inboxId': 'e5fc776c-a811-4cc5-9392-019f3872938b'
+        'inboxId': 'b6f7c995-d7b0-48e7-a1b6-9264b9598b37'
       }
     }
 );
