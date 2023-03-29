a11d757933fecb06a0ef6e98152005cdd4471d84
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-03-29 11:46:30 +0100

    Update error message mapping after BE refactor

--- src/constants/ErrorMappingFal5.js
+++ src/constants/ErrorMappingFal5.js
@@ -1,44 +1,46 @@
 const ErrorMappingFal5 = {
   A: { // travel document type
     order: 'a',
-    'none is not an allowed value': 'Enter P, I or O, or you may enter the full word: P - Passport, I - ID card - for a national ID card or O - Other - for any other document',
-    'Enter travel document as P, I, O or as Passport, ID card, Other': 'Enter travel document as P, I, O or as Passport, ID card, Other',
+    'field required': 'Enter P, I or O, or you may enter the full word: P - Passport, I - ID card - for a national ID card or O - Other - for any other document',
+    'travel document type must be 8 characters or less': 'Enter travel document as P, I, O or as Passport, ID card, Other',
   },
   B: { // other travel doument type
     order: 'b',
     "You entered 'Other' as the travel document type. You must enter the type of travel document, for example SID for Seafarer's Identity Document": "You entered 'Other' as the travel document type. You must enter the type of travel document, for example SID for Seafarer's Identity Document",
+    'travel document nature must be 35 characters or less': 'Enter the type of travel document in 35 characters or less',
   },
   C: { // travel document issuing country
     order: 'c',
-    'none is not an allowed value': 'Enter the 3-letter ISO country code for the issuing country; for example, GBR, SWE, NLD',
-    'Enter the country as a 3-letter ISO country code; for example, GBR, SWE, NLD': 'Enter the issuing country as a 3-letter ISO country code; for example, GBR, SWE, NLD',
+    'field required': 'Enter the 3-letter ISO country code for the issuing country; for example, GBR, SWE, NLD',
+    'ensure this value has at most 3 characters': 'Enter the issuing country as a 3-letter ISO country code; for example, GBR, SWE, NLD',
   },
   D: { // travel document number
     order: 'd',
-    'none is not an allowed value': 'Enter the number of the travel document',
+    'field required': 'Enter the number of the travel document',
     'Number of the travel document must be only numbers': 'Enter the number of the travel document using only numbers',
-    'ensure this value has at most 35 characters': 'Enter the number of the travel document in 35 characters or less',
+    'travel document number must be 35 characters or less': 'Enter the number of the travel document in 35 characters or less',
   },
   E: { // rank or rating
     order: 'e',
-    'none is not an allowed value': 'Enter the rank, rating or job title; for supernumeraries just put SN or supernumerary, not the job title',
+    'field required': 'Enter the rank, rating or job title; for supernumeraries just put SN or supernumerary, not the job title',
     'ensure this value has at most 35 characters': 'Enter the rank, rating or job title in 35 characters or less',
     'Enter the value using English letters instead of special characters not recognised': 'Enter the rank, rating or job title using English letters instead of special characters not recognised',
   },
   F: { // surname
     order: 'f',
-    'none is not an allowed value': 'Enter family name or surname as it appears in the travel document',
-    'ensure this value has at most 35 characters': 'Enter family name or surname in 35 characters or less',
+    'field required': 'Enter family name or surname as it appears in the travel document',
+    'surname must be 35 characters or less': 'Enter family name or surname in 35 characters or less',
     'Enter the value using English letters instead of special characters not recognised': 'Enter family name or surname using English letters instead of special characters not recognised',
   },
   G: { // forenames
     order: 'g',
-    'none is not an allowed value': 'Enter all forenames or given names as they appear in the travel document - if the crew member has no forename recorded enter UNKNOWN',
-    'ensure this value has at most 35 characters': 'Enter all forenames or given names in 35 characters or less',
-    'Enter the value using English letters instead of special characters not recognised': 'Enter forenames using English letters instead of special characters not recognised',
+    'field required': 'Enter all forenames or given names as they appear in the travel document - if the crew member has no forename recorded enter UNKNOWN',
+    'forenames must be 35 characters or less': 'Enter all forenames or given names in 35 characters or less',
+    'Enter the forenames using English letters instead of special characters not recognised': 'Enter forenames using English letters instead of special characters not recognised',
   },
   H: { // gender
     order: 'h',
+    'field required': 'Enter M for male, F for female, or X for gender neutral if this is in the travel document',
     'Enter M for male, F for female, or X for gender neutral if this is in the Travel Document': 'Enter M for male, F for female, or X for gender neutral if this is in the travel document',
   },
   I: { // date of birth
@@ -47,12 +49,12 @@ const ErrorMappingFal5 = {
   },
   J: { // place of birth
     order: 'j',
-    'ensure this value has at most 35 characters': 'Enter the place of birth in 35 characters or less',
+    'place of birth must be 35 characters or less': 'Enter the place of birth in 35 characters or less',
     'Enter the value using English letters instead of special characters not recognised': 'Enter place of birth using English letters instead of special characters not recognised',
   },
   K: { // nationality
     order: 'k',
-    'Enter the country as a 3-letter ISO country code; for example, GBR, SWE, NLD': 'Enter the nationality as a 3-letter ISO country code; for example, GBR, SWE, NLD',
+    'ensure this value has at most 3 characters': 'Enter the nationality as a 3-letter ISO country code; for example, GBR, SWE, NLD',
   },
   L: { // travel document expiry
     order: 'l',
