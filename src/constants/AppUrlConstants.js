439c8f6abfc44d7129f80971f23eff2d51ea675a
Jennifer Wallace <jennikate@gmail.com>
2023-04-12 13:58:52 +0100

    Add route

--- src/constants/AppUrlConstants.js
+++ src/constants/AppUrlConstants.js
@@ -13,6 +13,7 @@ export const REGISTER_EMAIL_RESEND_URL = '/create-account/request-new-verificati
 export const REGISTER_EMAIL_VERIFIED_URL = '/activate-account';
 export const REGISTER_DETAILS_URL = '/create-account/your-details';
 export const REGISTER_PASSWORD_URL = '/create-account/your-password';
+export const REQUEST_PASSWORD_RESET_URL = '/forgotten-password';
 export const SIGN_IN_PAGE_NAME = 'Sign in';
 export const SIGN_IN_URL = '/sign-in';
 export const LOGGED_IN_LANDING = '/your-voyages';
