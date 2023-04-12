070133eae21bf2b17d06573aeee4126d73b7a790
Jennifer Wallace <jennikate@gmail.com>
2023-04-12 15:28:09 +0100

    Add API call and handle error case

--- src/pages/SignIn/RequestPasswordReset.jsx
+++ src/pages/SignIn/RequestPasswordReset.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { REQUEST_PASSSWORD_RESET_ENDPOINT } from '../../constants/AppAPIConstants';
 import {
   FIELD_EMAIL,
   SINGLE_PAGE_FORM,
   VALIDATE_EMAIL_ADDRESS,
   VALIDATE_REQUIRED,
 } from '../../constants/AppConstants';
+import { MESSAGE_URL, REQUEST_PASSWORD_RESET_URL } from '../../constants/AppUrlConstants';
 import DisplayForm from '../../components/DisplayForm';
-// import Auth from '../../utils/Auth';
 
 const SupportingText = () => (
   <div>
@@ -17,7 +18,7 @@ const SupportingText = () => (
 );
 
 const RequestPasswordReset = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   document.title = 'Forgot password';
 
@@ -44,22 +45,21 @@ const RequestPasswordReset = () => {
     },
   ];
 
-  const requestPasswordResetEmail = async (emailToSendTo) => {
+  const requestPasswordResetEmail = async ({ emailToSendTo }) => {
     try {
       console.log('send password reset email', emailToSendTo);
-      // const controller = new AbortController();
-      // const response = await axios.post(ENDPOINT_HERE, {
-      //   email: emailToSendTo,
-      // }, {
-      //   signal: controller.signal,
-      // });
-
+      const controller = new AbortController();
+      const response = await axios.post(REQUEST_PASSSWORD_RESET_ENDPOINT, {
+        email: emailToSendTo,
+      }, {
+        signal: controller.signal,
+      });
+      console.log('r', response)
       // if (response.status === 204 || 401) {
       //   navigate(CONFIRMATION_URL_OR_COMPONENT_HERE, { state: { dataToSubmit: { emailAddress: emailToSendTo } } });
       // }
     } catch (err) {
-      // // 400 bad request, 500
-      // navigate(MESSAGE_URL, { state: { title: 'Something has gone wrong', message: err.response?.data?.message, redirectURL: PASSWORD_RESET_EMAIL_HERE } });
+      navigate(MESSAGE_URL, { state: { title: 'Something has gone wrong', message: err.response?.data?.message, redirectURL: REQUEST_PASSWORD_RESET_URL } });
     } finally {
       setIsLoading(false);
     }
