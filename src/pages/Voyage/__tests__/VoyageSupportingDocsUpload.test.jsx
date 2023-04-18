edacb5c1a2922b39956047ba397c0aab7f56f08a
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-04-18 14:13:37 +0100

    Update existing VoyageSupportingDocsUpload tests to accomodate new code

--- src/pages/Voyage/__tests__/VoyageSupportingDocsUpload.test.jsx
+++ src/pages/Voyage/__tests__/VoyageSupportingDocsUpload.test.jsx
@@ -1,12 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import {
   URL_DECLARATIONID_IDENTIFIER,
   VOYAGE_SUPPORTING_DOCS_UPLOAD_URL,
   VOYAGE_TASK_LIST_URL,
   YOUR_VOYAGES_URL,
 } from '../../../constants/AppUrlConstants';
+import {
+  API_URL,
+  ENDPOINT_DECLARATION_ATTACHMENTS_PATH,
+  ENDPOINT_DECLARATION_PATH
+} from '../../../constants/AppAPIConstants';
 import VoyageSupportingDocsUpload from '../VoyageSupportingDocsUpload';
 
 const mockUseLocationState = { state: {} };
@@ -27,13 +34,51 @@ const renderPage = () => {
 };
 
 describe('Voyage supporting docs page', () => {
+  const mockAxios = new MockAdapter(axios);
+
+  const mockedFAL1Response = {
+    FAL1: {
+      nameOfShip: 'Test ship name',
+      imoNumber: '1234567',
+      callSign: 'NA',
+      signatory: 'Captain Name',
+      flagState: 'GBR',
+      departureFromUk: false,
+      departurePortUnlocode: 'AUPOR',
+      departureDate: '2023-02-12',
+      departureTime: '09:23:00',
+      arrivalPortUnlocode: 'GBDOV',
+      arrivalDate: '2023-02-15',
+      arrivalTime: '14:00:00',
+      previousPortUnlocode: 'AUPOR',
+      nextPortUnlocode: 'NLRTM',
+      cargo: 'No cargo',
+      passengers: false,
+      creationDate: '2023-02-10',
+      submissionDate: '2023-02-11',
+    },
+    FAL5: [],
+    FAL6: [],
+    supporting: [],
+  };
+
   beforeEach(() => {
+    mockAxios.reset();
     window.sessionStorage.clear();
     mockUseLocationState.state = {};
   });
 
   it('should render the page with declarationId in url params', async () => {
+    mockAxios
+      .onGet(`${API_URL}${ENDPOINT_DECLARATION_PATH}/123${ENDPOINT_DECLARATION_ATTACHMENTS_PATH}`, {
+        headers: {
+          Authorization: 'Bearer 123',
+        },
+      })
+      .reply(200, mockedFAL1Response);
     renderPage();
+    await waitForElementToBeRemoved(() => screen.queryByText('Loading'));
+
     expect(screen.getByRole('heading', { name: 'Upload supporting documents' })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Upload files' }).outerHTML).toEqual('<button class="govuk-button govuk-button--secondary" type="button">Upload files</button>');
     expect(screen.getByRole('button', { name: 'Save and continue' }).outerHTML).toEqual('<button class="govuk-button govuk-button--primary" type="button">Save and continue</button>');
@@ -52,7 +97,16 @@ describe('Voyage supporting docs page', () => {
 
   it('should go to task details on save and continue click', async () => {
     const user = userEvent.setup();
+    mockAxios
+      .onGet(`${API_URL}${ENDPOINT_DECLARATION_PATH}/123${ENDPOINT_DECLARATION_ATTACHMENTS_PATH}`, {
+        headers: {
+          Authorization: 'Bearer 123',
+        },
+      })
+      .reply(200, mockedFAL1Response);
     renderPage();
+    await waitForElementToBeRemoved(() => screen.queryByText('Loading'));
+
     expect(screen.getByRole('heading', { name: 'Upload supporting documents' })).toBeInTheDocument();
     await user.click(screen.getByRole('button', { name: 'Save and continue' }));
     expect(mockedUseNavigate).toHaveBeenCalledWith(`${VOYAGE_TASK_LIST_URL}?${URL_DECLARATIONID_IDENTIFIER}=123`);
