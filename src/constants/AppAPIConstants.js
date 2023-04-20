65328e6647b64c61a269cf8954c8df9552fd6201
Jennifer Wallace <jennikate@gmail.com>
2023-04-20 16:50:05 +0100

    Add new error const

--- src/constants/AppAPIConstants.js
+++ src/constants/AppAPIConstants.js
@@ -20,6 +20,7 @@ export const ENDPOINT_FILE_UPLOAD_SUPPORTING_DOCUMENTS_PATH = '/supporting';
 export const ENDPOINT_DECLARATION_ATTACHMENTS_PATH = '/attachments';
 
 // Responses
+export const DUPLICATE_RECORDS = 'Duplicate person records found. Please check the upload (note that duplication could be in FAL5).';
 export const FILE_MISSING = 'No file provided';
 export const FILE_TYPE_INVALID_PREFIX = 'Invalid file type';
 export const FAL5_IS_EMPTY = 'File has no crew data';
