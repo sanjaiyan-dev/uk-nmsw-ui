90831ee6733828f91c5043dc892cb700ead605c8
Jennifer Wallace <jennikate@gmail.com>
2023-04-20 11:22:45 +0100

    Update tests

--- src/pages/Regulatory/__tests__/AccessibilityStatement.test.jsx
+++ src/pages/Regulatory/__tests__/AccessibilityStatement.test.jsx
@@ -7,33 +7,45 @@ describe('Accessibility statement tests', () => {
     expect(screen.getByText('Accessibility statement for National Maritime Single Window')).toBeInTheDocument();
   });
 
-  it('should render link to site title as stated in page constants', async () => {
+  it('should render link to site title', async () => {
     render(<AccessibilityStatement />);
-    expect(screen.getByRole('link', { name: 'How to use the UK national maritime portal for submitting pre-arrival reports - GOV.UK' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'How to use the UK national maritime portal for submitting pre-arrival reports - GOV.UK' }).outerHTML).toBe('<a href="https://www.gov.uk/government/publications/uk-national-maritime-single-window-launch-of-pilot" target="_blank" rel="noopener noreferrer">How to use the UK national maritime portal for submitting pre-arrival reports - GOV.UK</a>');
+    expect(screen.getByRole('link', { name: 'National Maritime Single Window' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'National Maritime Single Window' }).outerHTML).toBe('<a class="govuk-link" href="https://nmsw.homeoffice.gov.uk" target="_blank" rel="noopener noreferrer">National Maritime Single Window</a>');
+  });
+
+  it('should render link to service email', async () => {
+    render(<AccessibilityStatement />);
+    expect(screen.getByRole('link', { name: 'spcr@homeoffice.gov.uk' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'spcr@homeoffice.gov.uk' }).outerHTML).toBe('<a class="govuk-link" href="mailto: spcr@homeoffice.gov.uk">spcr@homeoffice.gov.uk</a>');
+  });
+
+  it('should render link to email to request alternative format as stated in page constants', async () => {
+    render(<AccessibilityStatement />);
+    expect(screen.getByRole('link', { name: 'alternativeformats@homeoffice.gov.uk' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'alternativeformats@homeoffice.gov.uk' }).outerHTML).toBe('<a class="govuk-link" href="mailto: alternativeformats@homeoffice.gov.uk">alternativeformats@homeoffice.gov.uk</a>');
   });
 
   it('should render link to AbilityNet as stated in page constants', async () => {
     render(<AccessibilityStatement />);
     expect(screen.getByRole('link', { name: 'AbilityNet' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'AbilityNet' }).outerHTML).toBe('<a href="https://mcmw.abilitynet.org.uk" target="_blank" rel="noopener noreferrer">AbilityNet</a>');
+    expect(screen.getByRole('link', { name: 'AbilityNet' }).outerHTML).toBe('<a class="govuk-link" href="https://mcmw.abilitynet.org.uk" target="_blank" rel="noopener noreferrer">AbilityNet</a>');
   });
 
   it('should render link to a complaint escalation site as stated in page constants', async () => {
     render(<AccessibilityStatement />);
     expect(screen.getByRole('link', { name: 'contact the Equality Advisory and Support Website (EASS)' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'contact the Equality Advisory and Support Website (EASS)' }).outerHTML).toBe('<a href="https://www.equalityadvisoryservice.com" target="_blank" rel="noopener noreferrer">contact the Equality Advisory and Support Website (EASS)</a>');
+    expect(screen.getByRole('link', { name: 'contact the Equality Advisory and Support Website (EASS)' }).outerHTML).toBe('<a class="govuk-link" href="https://www.equalityadvisoryservice.com" target="_blank" rel="noopener noreferrer">contact the Equality Advisory and Support Website (EASS)</a>');
   });
 
   it('should render link ta complaint escalation site for Northern Ireland as stated in page constants', async () => {
     render(<AccessibilityStatement />);
     expect(screen.getByRole('link', { name: 'Equalities Commission for Northern Ireland' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'Equalities Commission for Northern Ireland' }).outerHTML).toBe('<a href="https://www.equalityni.org/Home" target="_blank" rel="noopener noreferrer">Equalities Commission for Northern Ireland</a>');
+    expect(screen.getByRole('link', { name: 'Equalities Commission for Northern Ireland' }).outerHTML).toBe('<a class="govuk-link" href="https://www.equalityni.org/Home" target="_blank" rel="noopener noreferrer">Equalities Commission for Northern Ireland</a>');
   });
 
   it('should render link to WCAG as stated in page constants', async () => {
     render(<AccessibilityStatement />);
     expect(screen.getByRole('link', { name: 'Web Content Accessibility Guidelines version 2.1' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'Web Content Accessibility Guidelines version 2.1' }).outerHTML).toBe('<a href="https://www.w3.org/TR/WCAG21" target="_blank" rel="noopener noreferrer">Web Content Accessibility Guidelines version 2.1</a>');
+    expect(screen.getByRole('link', { name: 'Web Content Accessibility Guidelines version 2.1' }).outerHTML).toBe('<a class="govuk-link" href="https://www.w3.org/TR/WCAG21" target="_blank" rel="noopener noreferrer">Web Content Accessibility Guidelines version 2.1</a>');
   });
 });
