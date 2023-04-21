d657f6ec01d93c425b9eb6b8e51b05a7d3327cd9
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-04-21 17:07:38 +0100

    Update landing page with new content

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { SERVICE_NAME } from '../../constants/AppConstants';
+import { SERVICE_CONTACT_EMAIL, SERVICE_NAME } from '../../constants/AppConstants';
 import { YOUR_VOYAGES_URL, REGISTER_ACCOUNT_URL, SIGN_IN_URL } from '../../constants/AppUrlConstants';
 import useUserIsPermitted from '../../hooks/useUserIsPermitted';
 import { DownloadFile } from '../../utils/DownloadFile';
@@ -12,6 +12,8 @@ const Landing = () => {
     <div className="govuk-grid-row">
       <div className="govuk-grid-column-two-thirds">
         <h1 className="govuk-heading-l" data-testid="landing-h1">{SERVICE_NAME}</h1>
+        <h2 className="govuk-heading-m">Who should use this service</h2>
+        <p className="govuk-body">The NMSW service is for General Maritime commercial shipping on uncanalised international voyages.</p>
         <p className="govuk-body">Use this service to tell Border Force about your crew and passengers, each time your vessel will depart from or arrive in the UK.</p>
         <h2 className="govuk-heading-m">Before you start</h2>
         <p className="govuk-body">You can save a partially completed report. To complete a full report you’ll need:</p>
@@ -28,6 +30,11 @@ const Landing = () => {
             <button className="govuk-button--text text-align--left" type="button" onClick={() => DownloadFile('assets/files/Passenger details FAL 6.xlsx', 'Passenger details FAL 6.xlsx')}>Passenger details (FAL 6) template (xlsx, 90kb)</button>
           </li>
         </ul>
+        <div className="govuk-inset-text">
+          <p className="govuk-body">
+            You do not need to submit any FAL forms that you send to other UK authorities, such as the Consolidated European Reporting System, CERS.
+          </p>
+        </div>
         <p className="govuk-body" data-testid="createAccountParagraph">You&apos;ll also need to sign in or <Link to={REGISTER_ACCOUNT_URL}>create an account</Link> to use this service</p>
         <Link
           to={isAuthenticated ? YOUR_VOYAGES_URL : SIGN_IN_URL}
@@ -49,6 +56,13 @@ const Landing = () => {
             <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
           </svg>
         </Link>
+        <h2 className="govuk-heading-m">More help with this service</h2>
+        <p className="govuk-body">
+          {`If you need more support, email `}
+          <a className="govuk-link" href={`mailto: ${SERVICE_CONTACT_EMAIL}`}>
+            {SERVICE_CONTACT_EMAIL}
+          </a>
+        </p>
       </div>
     </div>
   );
