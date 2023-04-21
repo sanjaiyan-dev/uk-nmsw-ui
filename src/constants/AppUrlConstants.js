95453b142eb7886828bf1a335b01fa60a60595f3
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-04-21 16:51:57 +0100

    Update contact constant to be consistent

--- src/constants/AppUrlConstants.js
+++ src/constants/AppUrlConstants.js
@@ -26,7 +26,7 @@ export const ACCESSIBILITY_URL = '/accessibility-statement';
 export const COOKIE_URL = '/cookies';
 export const LANDING_URL = '/';
 export const PRIVACY_URL = '/privacy-notice';
-export const CONTACT_US = '/contact-us'
+export const CONTACT_US_URL = '/contact-us'
 
 // Voyage pages
 export const URL_DECLARATIONID_IDENTIFIER = 'report';
