f52dffb72fd465b593b661d224b0641b823db873
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-04-25 11:20:28 +0100

    Test password reset and sign-in feature for email address not registered

--- cypress/e2e/features/password-reset.feature
+++ cypress/e2e/features/password-reset.feature
@@ -30,6 +30,8 @@ Feature: User able to reset password
     Then I am taken to new password page
     When I enter a new password and confirmation password that matches the new password
     Then I am shown the message - Password reset link has expired
+    And I click `Request New Link` button
+    Then I am taken to forgotten-password page
 
   Scenario: User is redirected as expected invalid or missing token
     When user tries to reset password with missing token
@@ -61,3 +63,18 @@ Feature: User able to reset password
     Then I am shown corresponding error message
       | Field | emailAddress-error                |
       | Error | Error: Enter a real email address |
+
+  Scenario: User should not receive email for email address not registered
+    When I click Forgotten your password? link
+    Then I am taken to forgotten-password page
+    Then I enter an email address that is not registered
+    Then I am shown check your email page but email should not be sent
+
+  Scenario: User able to request password reset link for unverified email address
+    When I click Forgotten your password? link
+    Then I am taken to forgotten-password page
+    Then I enter the email that is registered but not verified
+    And I click send the link
+    Then I shown email address has not been verified
+    When I click send verification email
+#    Then I am taken to check your email page
--- cypress/support/step_definitions/password-reset.steps.js
+++ cypress/support/step_definitions/password-reset.steps.js
@@ -5,103 +5,131 @@ import BasePage from "../../e2e/pages/base.page";
 import SignInPage from "../../e2e/pages/sign-in.page";
 
 When('I click Forgotten your password? link', () => {
-  PasswordPage.clickForgotPasswordLink();
+    PasswordPage.clickForgotPasswordLink();
 });
 
 Then('I am taken to forgotten-password page', () => {
-  PasswordPage.checkForgottenPasswordPage();
+    PasswordPage.checkForgottenPasswordPage();
 });
 
 When('I enter my email to request a forgotten password link', () => {
-  cy.fixture('registration.json').then((registration) => {
-    EmailPage.enterEmailAddress(registration.signInEmail2);
-  })
+    cy.fixture('registration.json').then((registration) => {
+        EmailPage.enterEmailAddress(registration.signInEmail2);
+    })
 });
 
 Then('I click send the link', () => {
-  PasswordPage.clickSendLinkBtn();
+    PasswordPage.clickSendLinkBtn();
 });
 
 When('I click the password reset link received', () => {
-  cy.waitForLatestEmail('7ee68e7d-c48e-438c-8422-c09bfe264e13').then((mail) => {
-    assert.isDefined(mail);
-    const token = /token=([A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*)/.exec(mail.body)[1];
-    cy.wrap(token).as('token');
-    cy.visitUrl(`/new-password?token=${token}`);
-  });
+    cy.waitForLatestEmail('7ee68e7d-c48e-438c-8422-c09bfe264e13').then((mail) => {
+        assert.isDefined(mail);
+        const token = /token=([A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*)/.exec(mail.body)[1];
+        cy.wrap(token).as('token');
+        cy.intercept('PATCH', '**/reset-password').as('passwordReset');
+        cy.visitUrl(`/new-password?token=${token}`);
+        // cy.wait('@passwordReset').then(({response}) => {
+        //     expect(response.statusCode).to.equal(204);
+        // });
+    });
 });
 
 Then('I am taken to new password page', () => {
-  PasswordPage.checkNewPasswordPage();
+    PasswordPage.checkNewPasswordPage();
 });
 
 When('I click Reset password button without providing password details', () => {
-  PasswordPage.clickResetPassword();
+    PasswordPage.clickResetPassword();
 });
 
 When('I enter a new password', () => {
-  PasswordPage.typePassword('NewPassword');
+    PasswordPage.typePassword('NewPassword');
 });
 
 Then('I enter a confirmation password that is different to the new password', () => {
-  PasswordPage.typeRepeatPassword('OddPassword');
-  PasswordPage.clickResetPassword();
+    PasswordPage.typeRepeatPassword('OddPassword');
+    PasswordPage.clickResetPassword();
 });
 
 When('I enter a new password and confirmation password that matches the new password', () => {
-  PasswordPage.typePassword('NewPassword');
-  PasswordPage.typeRepeatPassword('NewPassword');
-  cy.intercept('POST', '/new-password?token=').as('verifyPasswordReset');
-  PasswordPage.clickResetPassword();
+    PasswordPage.typePassword('NewPassword');
+    PasswordPage.typeRepeatPassword('NewPassword');
+    cy.intercept('POST', '/new-password?token=').as('verifyPasswordReset');
+    PasswordPage.clickResetPassword();
 });
 
 Then('I can see the confirmation screen with sign in link', () => {
-  BasePage.checkH1('Your password has been reset');
-  PasswordPage.clickPwdSignIn();
+    BasePage.checkH1('Your password has been reset');
+    PasswordPage.clickPwdSignIn();
 });
 
 When('I sign-in with old password', () => {
-  SignInPage.defaultSignIn();
+    SignInPage.defaultSignIn();
 });
 
 When('I sign-in with new password', () => {
-  cy.fixture('registration.json').then((registration) => {
-    SignInPage.enterEmailAddress(registration.signInEmail2);
-  });
-  SignInPage.enterPassword('NewPassword');
-  SignInPage.clickSignIn();
+    cy.fixture('registration.json').then((registration) => {
+        SignInPage.enterEmailAddress(registration.signInEmail2);
+    });
+    SignInPage.enterPassword('NewPassword');
+    SignInPage.clickSignIn();
 });
 
 Then('I am shown the message - Password reset link has expired', () => {
-  BasePage.checkH1('Password reset link has expired');
+    BasePage.checkH1('Password reset link has expired');
 });
 
 When('user tries to reset password with missing token', () => {
-  cy.visitUrl('/new-password?token=');
+    cy.visitUrl('/new-password?token=');
 });
 
 When('I click the same password reset link again', () => {
-  cy.get('@token').then((token) => {
-    cy.visitUrl(`/new-password?token=${token}`);
-  })
+    cy.get('@token').then((token) => {
+        cy.visitUrl(`/new-password?token=${token}`);
+    })
 });
 
 When('user tries to reset password with invalid token', () => {
-  cy.visitUrl('/new-password?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjRmM2E1ZDg1LTk5YmQtNDZkYi1iOGVhLTgwZWE4NzcyYzljNUBtYWlsc2x1cnAuY29tIiwiZXhwIjoxNjgxOTIzNDgwLCJqaXQiOiIyZT');
+    cy.visitUrl('/new-password?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjRmM2E1ZDg1LTk5YmQtNDZkYi1iOGVhLTgwZWE4NzcyYzljNUBtYWlsc2x1cnAuY29tIiwiZXhwIjoxNjgxOTIzNDgwLCJqaXQiOiIyZT');
 });
 
 Then('the user is redirected to request-new-password-link', () => {
-  cy.url().should('include', 'request-new-link');
+    cy.url().should('include', 'request-new-link');
 });
 
 Then('I click change the email sent link', () => {
-  cy.contains('Change where the email was sent').click();
+    cy.contains('Change where the email was sent').click();
 });
 
 Then('I change to different email previously registered', () => {
-  EmailPage.enterEmailAddress('jaybworkemail2@gmail.com')
+    EmailPage.enterEmailAddress('jaybworkemail2@gmail.com')
 });
 
 When('I enter the email in invalid format', () => {
-  EmailPage.enterEmailAddress('randomemail@mail');
-})
+    EmailPage.enterEmailAddress('randomemail@mail');
+});
+
+When('I enter the email that is registered but not verified', () => {
+    EmailPage.enterEmailAddress('e5fc776c-a811-4cc5-9392-019f3872938b@mailslurp.com')
+});
+
+Then('I shown email address has not been verified', () => {
+    BasePage.checkH1('Email address not verified');
+});
+
+When('I click send verification email', () => {
+    BasePage.clickSendConfirmationEmail();
+});
+
+When('I enter an email address that is not registered', () => {
+    EmailPage.enterEmailAddress('test@test.com')
+});
+
+Then('I am shown check your email page but email should not be sent', () => {
+    cy.intercept('POST', '**/reset-password').as('verifyReg');
+    PasswordPage.clickSendLinkBtn();
+    cy.wait('@verifyReg').then(({response}) => {
+        expect(response.statusCode).to.be.oneOf([401, 400]);
+    });
+});
