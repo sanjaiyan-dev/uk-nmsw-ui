5ec6bcb7c97c743427e2720654b7883c56979927
Jennifer Wallace <jennikate@gmail.com>
2023-04-26 10:19:15 +0100

    Add tests

--- src/pages/NavPages/YourDetails/__tests__/YourDetails.test.jsx
+++ src/pages/NavPages/YourDetails/__tests__/YourDetails.test.jsx
@@ -9,7 +9,7 @@ import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import YourDetails from '../YourDetails';
-import { TOKEN_EXPIRED, USER_ENDPOINT } from '../../../../constants/AppAPIConstants';
+import { GROUP_ENDPOINT, TOKEN_EXPIRED, USER_ENDPOINT } from '../../../../constants/AppAPIConstants';
 import { MESSAGE_URL, SIGN_IN_URL, YOUR_DETAILS_PAGE_URL } from '../../../../constants/AppUrlConstants';
 
 const mockedUseNavigate = jest.fn();
@@ -17,6 +17,31 @@ jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
   useNavigate: () => mockedUseNavigate,
 }));
+
+const userResponse = {
+  userId: '123',
+  email: 'useremail@test.com',
+  fullName: 'Bob Doe',
+  phoneNumber: '(44)123456789',
+  countryCode: 'GBR',
+  verified: true,
+  dateCreated: '2023-02-06T16:21:09.958032',
+  lastUpdated: '2023-04-18T09:09:54.759977',
+  userType: {
+    userTypeId: '321',
+    name: 'Admin',
+  },
+  groupId: '456',
+};
+const groupResponse = {
+  groupId: 'abc',
+  groupName: 'Company Name',
+  website: null,
+  dateCreated: '2023-02-06T16:21:09.633696',
+  typeOfCompany: 'Shipping Agency',
+  lastUpdated: '2023-02-07T08:21:09.667603',
+  groupType: null,
+};
 
 describe('Your details tests', () => {
   const mockAxios = new MockAdapter(axios);
@@ -41,24 +66,12 @@ describe('Your details tests', () => {
     expect(screen.getByRole('link', { name: 'Change your password' })).toBeInTheDocument();
   });
 
-  it('should render user details if success response', async () => {
+  it('should render user & group details if success response', async () => {
     mockAxios
       .onGet(USER_ENDPOINT)
-      .reply(200, {
-        userId: '123',
-        email: 'useremail@test.com',
-        fullName: 'Bob Doe',
-        phoneNumber: '(44)123456789',
-        countryCode: 'GBR',
-        verified: true,
-        dateCreated: '2023-02-06T16:21:09.958032',
-        lastUpdated: '2023-04-18T09:09:54.759977',
-        userType: {
-          userTypeId: '321',
-          name: 'Admin',
-        },
-        groupId: '456',
-      });
+      .reply(200, userResponse)
+      .onGet(GROUP_ENDPOINT)
+      .reply(200, groupResponse);
 
     render(<MemoryRouter><YourDetails /></MemoryRouter>);
     await waitForElementToBeRemoved(() => screen.queryByText('Loading'));
@@ -67,11 +80,28 @@ describe('Your details tests', () => {
     expect(screen.getByText('(44)123456789')).toBeInTheDocument();
     expect(screen.getByText('GBR')).toBeInTheDocument();
     expect(screen.getByText('Admin')).toBeInTheDocument();
+    expect(screen.getByText('Company Name')).toBeInTheDocument();
+    expect(screen.getByText('Shipping Agency')).toBeInTheDocument();
   });
 
-  it('should redirect to sign in if getting the declarations returns a 422', async () => {
+  it('should redirect to sign in if getting the user declarations returns a 422', async () => {
     mockAxios
       .onGet(USER_ENDPOINT)
+      .reply(422, { msg: 'Not enough segments' })
+      .onGet(GROUP_ENDPOINT)
+      .reply(200, groupResponse);
+    render(<MemoryRouter><YourDetails /></MemoryRouter>);
+
+    await waitFor(() => {
+      expect(mockedUseNavigate).toHaveBeenCalledWith(SIGN_IN_URL, { state: { redirectURL: YOUR_DETAILS_PAGE_URL } });
+    });
+  });
+
+  it('should redirect to sign in if getting the group declarations returns a 422', async () => {
+    mockAxios
+      .onGet(USER_ENDPOINT)
+      .reply(200, userResponse)
+      .onGet(GROUP_ENDPOINT)
       .reply(422, { msg: 'Not enough segments' });
     render(<MemoryRouter><YourDetails /></MemoryRouter>);
 
@@ -80,9 +110,24 @@ describe('Your details tests', () => {
     });
   });
 
-  it('should redirect to sign in if getting the declarations returns a 401 token expired', async () => {
+  it('should redirect to sign in if getting the user declarations returns a 401 token expired', async () => {
     mockAxios
       .onGet(USER_ENDPOINT)
+      .reply(401, { msg: TOKEN_EXPIRED })
+      .onGet(GROUP_ENDPOINT)
+      .reply(200, groupResponse);
+    render(<MemoryRouter><YourDetails /></MemoryRouter>);
+
+    await waitFor(() => {
+      expect(mockedUseNavigate).toHaveBeenCalledWith(SIGN_IN_URL, { state: { redirectURL: YOUR_DETAILS_PAGE_URL } });
+    });
+  });
+
+  it('should redirect to sign in if getting the group declarations returns a 401 token expired', async () => {
+    mockAxios
+      .onGet(USER_ENDPOINT)
+      .reply(200, userResponse)
+      .onGet(GROUP_ENDPOINT)
       .reply(401, { msg: TOKEN_EXPIRED });
     render(<MemoryRouter><YourDetails /></MemoryRouter>);
 
@@ -91,9 +136,24 @@ describe('Your details tests', () => {
     });
   });
 
-  it('should redirect to message page on other errors', async () => {
+  it('should redirect to message page on other user endpoint errors', async () => {
     mockAxios
       .onGet(USER_ENDPOINT)
+      .reply(500)
+      .onGet(GROUP_ENDPOINT)
+      .reply(200, groupResponse);
+    render(<MemoryRouter><YourDetails /></MemoryRouter>);
+
+    await waitFor(() => {
+      expect(mockedUseNavigate).toHaveBeenCalledWith(MESSAGE_URL, { state: { title: 'Something has gone wrong', undefined, redirectURL: YOUR_DETAILS_PAGE_URL } });
+    });
+  });
+
+  it('should redirect to message page on group endpoint other errors', async () => {
+    mockAxios
+      .onGet(USER_ENDPOINT)
+      .reply(200, userResponse)
+      .onGet(GROUP_ENDPOINT)
       .reply(500);
     render(<MemoryRouter><YourDetails /></MemoryRouter>);
 
