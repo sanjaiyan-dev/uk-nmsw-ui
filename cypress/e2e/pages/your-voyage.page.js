6080bb0a3413ba387ea4ef3e51c6e1cad1d51d7f
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-04-26 17:12:16 +0100

    Test NMSW-552 Handle duplicate person error

--- cypress/e2e/pages/your-voyage.page.js
+++ cypress/e2e/pages/your-voyage.page.js
@@ -8,7 +8,7 @@ class YourVoyagePage {
     cy.contains('All report types');
     // cy.get('.govuk-heading-s').should('have.text', '1 reported voyages');
     cy.get(':nth-child(1) > :nth-child(2) > .govuk-grid-row').each(($row) => {
-      cy.wrap($row).should('have.text', 'NMSW Test ShipVoyage type:Arrival to the UKDate:15 February 2023Status draftActions Continue');
+      cy.wrap($row).should('have.text', 'NMSW Test ShipVoyage type:Arrival to the UKDate:15 February 2023Status DraftActions Continue');
     })
   }
 }
