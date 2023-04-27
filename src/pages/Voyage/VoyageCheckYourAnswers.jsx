390f283bc18f05d1e0df6ca378d041248325fdcb
Jennifer Wallace <jennikate@gmail.com>
2023-04-27 10:01:34 +0100

    Only show change links if status is Draft

--- src/pages/Voyage/VoyageCheckYourAnswers.jsx
+++ src/pages/Voyage/VoyageCheckYourAnswers.jsx
@@ -319,18 +319,20 @@ const VoyageCheckYourAnswers = () => {
               </dt>
               <dd className="govuk-summary-list__value" />
               <dd className="govuk-summary-list__actions">
-                <Link
-                  className="govuk-link"
-                  to={`${VOYAGE_GENERAL_DECLARATION_UPLOAD_URL}?${URL_DECLARATIONID_IDENTIFIER}=${declarationId}`}
-                  aria-describedby="voyageDetails"
-                >
-                  Change<span className="govuk-visually-hidden"> change voyage details</span>
-                </Link>
+                {declarationStatus?.status === DECLARATION_STATUS_DRAFT
+                  && (
+                    <Link
+                      className="govuk-link"
+                      to={`${VOYAGE_GENERAL_DECLARATION_UPLOAD_URL}?${URL_DECLARATIONID_IDENTIFIER}=${declarationId}`}
+                      aria-describedby="voyageDetails"
+                    >
+                      Change<span className="govuk-visually-hidden"> change voyage details</span>
+                    </Link>
+                  )}
               </dd>
             </div>
 
-            {
-              declarationStatus?.status !== DECLARATION_STATUS_DRAFT
+            {declarationStatus?.status !== DECLARATION_STATUS_DRAFT
               && (
                 <div className="govuk-summary-list__row">
                   <dt className="govuk-summary-list__key">
@@ -340,8 +342,7 @@ const VoyageCheckYourAnswers = () => {
                     <StatusTag status={declarationStatus?.status} /> {declarationStatus?.submissionDate}
                   </dd>
                 </div>
-              )
-            }
+              )}
 
             {voyageDetails.map((item) => (
               <div key={item.title} className="govuk-summary-list__row">
@@ -376,13 +377,16 @@ const VoyageCheckYourAnswers = () => {
                   {item.fileLink ? <a className="govuk-link" href={item.fileLink} download>{item.value}</a> : item.noFileText}
                 </dd>
                 <dd className="govuk-summary-list__actions">
-                  <Link
-                    className="govuk-link"
-                    to={item.changeLink}
-                    aria-describedby={item.id}
-                  >
-                    Change<span className="govuk-visually-hidden">{` change ${item.title}`}</span>
-                  </Link>
+                  {declarationStatus?.status === DECLARATION_STATUS_DRAFT
+                    && (
+                      <Link
+                        className="govuk-link"
+                        to={item.changeLink}
+                        aria-describedby={item.id}
+                      >
+                        Change<span className="govuk-visually-hidden">{` change ${item.title}`}</span>
+                      </Link>
+                    )}
                 </dd>
               </div>
             ))}
@@ -402,13 +406,16 @@ const VoyageCheckYourAnswers = () => {
                 }
               </dd>
               <dd className="govuk-summary-list__actions">
-                <Link
-                  className="govuk-link"
-                  to={`${VOYAGE_SUPPORTING_DOCS_UPLOAD_URL}?${URL_DECLARATIONID_IDENTIFIER}=${declarationId}`}
-                  aria-describedby="supportingDocuments"
-                >
-                  Change<span className="govuk-visually-hidden">{' change Supporting documents'}</span>
-                </Link>
+                {declarationStatus?.status === DECLARATION_STATUS_DRAFT
+                  && (
+                    <Link
+                      className="govuk-link"
+                      to={`${VOYAGE_SUPPORTING_DOCS_UPLOAD_URL}?${URL_DECLARATIONID_IDENTIFIER}=${declarationId}`}
+                      aria-describedby="supportingDocuments"
+                    >
+                      Change<span className="govuk-visually-hidden">{' change Supporting documents'}</span>
+                    </Link>
+                  )}
               </dd>
             </div>
           </dl>
