c815e9323b8a3bda33dc72444059377225fb5ffc
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-05-05 01:41:31 +0100

    Changed CYA page steps wording and added Save and Submit functionality

--- cypress/support/step_definitions/check-your-answers-page.steps.js
+++ cypress/support/step_definitions/check-your-answers-page.steps.js
@@ -8,7 +8,7 @@ When('I click Check answers and submit', () => {
   cyaPage.clickCheckAnswersAndSubmit();
 });
 
-Then('I can verify Check Your Answers page', () => {
+Then('I am taken to Check Your Answers page', () => {
   cyaPage.verifyCYAPage();
   cyaPage.verifySaveAndSubmitButton();
   cyaPage.verifyH2Headings();
@@ -21,18 +21,15 @@ Then('I can view Check Your Answers page', () => {
 
 Then('the details from my FAL 1 form are displayed on CYA page', () => {
   const fieldKeyValue = [
-    {key: 'Voyage type', value: "Arrival to the UK"},
+    {key: 'Voyage type', value: "Departure from the UK"},
     {key: 'Ship name', value: "NMSW Test Ship"},
     {key: 'IMO number', value: "9999990"},
     {key: 'Call sign', value: "1234"},
     {key: 'Flag state of ship', value: "Canada"},
-    {
-      key: 'Departure details',
-      value: "Departure port LOCODEUS  XXXDate of departure12 February 2023Time of departure13:00"
-    },
-    {key: 'Arrival details', value: "Arrival port LOCODEGB  XXXDate of arrival15 February 2023Time of arrival12:00"},
-    {key: 'Next port of call', value: "JP  NXX"},
-    {key: 'Brief description of the cargo', value: "No cargo"},
+    {key: 'Departure details', value: "Departure port LOCODEGB ABCDate of departure03 May 2023Time of departure01:00"},
+    {key: 'Arrival details', value: "Arrival port LOCODELK CMBDate of arrival15 October 2023Time of arrival12:00"},
+    {key: 'Next port of call', value: "LK CMB"},
+    {key: 'Brief description of the cargo', value: "Hardware and Textiles"},
   ];
 
   cy.get('dl:nth-child(1) .govuk-summary-list__row').each((row, index) => {
@@ -63,7 +60,7 @@ When('I click upload files', () => {
 });
 
 Then('I can see a link to an uploaded crew file {string}{string}', (folderName, fileName) => {
-CyaPage.verifyFileUploaded(fileName);
+  CyaPage.verifyFileUploaded(fileName);
 });
 
 When('I click on the file name for {string}, it is downloaded', (folderName) => {
@@ -84,7 +81,7 @@ Then('passenger section state No passenger details provided', () => {
 });
 
 When('I click on change next to Passenger details', () => {
-cy.get('#passengerDetails').parent().find('a').contains('Change').click();
+  cy.get('#passengerDetails').parent().find('a').contains('Change').click();
 });
 
 When('I click change next to supporting documents', () => {
@@ -97,10 +94,23 @@ When('I navigate back to check your answers page', () => {
   cy.url().should('include', 'report-voyage/check-your-answers?');
 });
 
-When('I click Save and Submit', () => {
+When('I click Save and Submit to confirm submission', () => {
+  cy.intercept('PATCH', '**/declaration/*').as('submitPatch');
   cyaPage.clickSaveAndSubmitButton();
+  cy.wait('@submitPatch').then(({response}) => {
+    expect(response.body.status).eq('PreSubmitted');
+    expect(response.statusCode).eq(202);
+  })
 });
 
 When('there is no supporting documents attached, I can see-no supporting documents message', () => {
   cy.get('#supportingDocuments').parent().find('span').contains('No supporting documents provided');
+});
+
+When('I click Save and Submit', () => {
+  cyaPage.clickSaveAndSubmitButton();
+});
+
+When('I click return to your voyages link', () => {
+  cy.get('.govuk-grid-column-two-thirds > a').should('have.text', 'Return to your voyages').click();
 });
