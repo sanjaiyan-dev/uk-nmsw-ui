fa7e1db7d9c20283a600d8d124c02fa2aa40546a
Adele Bampton <72464825+Adele-B@users.noreply.github.com>
2023-05-09 14:52:29 +0100

    Add DownloadTemplate to template page

--- src/pages/NavPages/Templates.jsx
+++ src/pages/NavPages/Templates.jsx
@@ -1,5 +1,5 @@
+import DownloadTemplates from '../../components/DownloadTemplates';
 import { SERVICE_NAME } from '../../constants/AppConstants';
-import { DownloadFile } from '../../utils/DownloadFile';
 
 const Templates = () => {
   document.title = SERVICE_NAME;
@@ -8,17 +8,7 @@ const Templates = () => {
       <div className="govuk-grid-column-two-thirds">
         <h1 className="govuk-heading-xl">Templates</h1>
         <p className="govuk-body">Use the templates provided:</p>
-        <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-7">
-          <li>
-            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/General declaration FAL 1.xlsx', 'General declaration FAL 1.xlsx')}>General Declaration (FAL 1) template (xlsx, 31kb)</button>
-          </li>
-          <li>
-            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/Crew details including supernumeraries FAL 5.xlsx', 'Crew details including supernumeraries FAL 5.xlsx')}>Crew details including supernumeraries (FAL 5) template (xlsx, 118kb)</button>
-          </li>
-          <li>
-            <button className="govuk-button--text" type="button" onClick={() => DownloadFile('assets/files/Passenger details FAL 6.xlsx', 'Passenger details FAL 6.xlsx')}>Passenger details (FAL 6) template (xlsx, 90kb)</button>
-          </li>
-        </ul>
+        <DownloadTemplates />
         <details className="govuk-details" data-module="govuk-details">
           <summary className="govuk-details__summary">
             <span className="govuk-details__summary-text">
