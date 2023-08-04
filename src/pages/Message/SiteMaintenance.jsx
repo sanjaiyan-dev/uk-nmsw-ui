d3abfcbed11f10d90c2d9c714395b5a9248ae73e
Jennifer Wallace <jennikate@gmail.com>
2023-08-04 12:42:46 +0100

    Add site maintenance content & test

--- src/pages/Message/SiteMaintenance.jsx
+++ src/pages/Message/SiteMaintenance.jsx
@@ -1,3 +1,5 @@
+import BorderForceRegionContacts from '../../components/BorderForceRegionContacts';
+import DownloadTemplates from '../../components/DownloadTemplates';
 import { SERVICE_NAME } from '../../constants/AppConstants';
 
 const SiteMaintenance = () => {
@@ -5,8 +7,15 @@ const SiteMaintenance = () => {
 
   return (
     <div className="govuk-grid-row">
-      <div className="govuk-grid-column-two-thirds">
-        <h1 className="govuk-heading-xl" data-testid="landing-h1">{SERVICE_NAME}</h1>
+      <div className="govuk-grid-column-full">
+        <h1 className="govuk-heading-xl" data-testid="landing-h1">Sorry, the service is unavailable</h1>
+        <h2 className="govuk-heading-m">If you need to submit your voyage report urgently</h2>
+        <p className="govuk-body">
+          Send the completed forms to the Border Force team that covers the region of the UK where your vessel will arrive or depart.
+        </p>
+        <p className="govuk-body">Use the templates provided:</p>
+        <DownloadTemplates />
+        <BorderForceRegionContacts />
       </div>
     </div>
   );
