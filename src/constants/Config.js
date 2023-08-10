b812a2eddab72df415e4d51a12c6c3310333d56e
Jennifer Wallace <jennikate@gmail.com>
2023-08-10 16:59:35 +0100

    Handle missing var

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -4,6 +4,5 @@ const apiBaseUrl = process.env.NMSW_DATA_API_BASE_URL || 'http://localhost:5000'
 // eslint-disable-next-line import/prefer-default-export
 export const apiUrl = `${apiBaseUrl}/${apiVersion}`;
 export const gaToken = process.env.GA_TOKEN;
-
 // for nmsw-ui when we recevie this variable it comes to us as a string
-export const inSiteMaintenance = process.env.NMSW_MAINTENANCE.toLowerCase() === 'true' || false;
+export const inSiteMaintenance = (process.env.NMSW_MAINTENANCE && process.env.NMSW_MAINTENANCE.toLowerCase()) === 'true' || false;
