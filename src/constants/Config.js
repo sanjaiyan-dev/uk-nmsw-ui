0ff40986f146eb17d99ac067d5b7852808bb4b8f
JenniKate Wallace <jennikate@gmail.com>
2023-08-10 16:44:06 +0100

    Convert maintenance var to bool

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -4,4 +4,5 @@ const apiBaseUrl = process.env.NMSW_DATA_API_BASE_URL || 'http://localhost:5000'
 // eslint-disable-next-line import/prefer-default-export
 export const apiUrl = `${apiBaseUrl}/${apiVersion}`;
 export const gaToken = process.env.GA_TOKEN;
-export const inSiteMaintenance = process.env.NMSW_MAINTENANCE || false;
+// for nmsw-ui when we recevie this variable it comes to us as a string
+export const inSiteMaintenance = process.env.NMSW_MAINTENANCE.toLowerCase() === 'true' || false;
