7e06eef1960958a1e0307b3542fd32604b3fb82a
Jayanthi <jayanthi.balakrishnan@digital.homeoffice.gov.uk>
2023-10-05 22:32:10 +0100

    Add validation test for FAL6 missing mandatory fields

--- cypress/support/step_definitions/fal6-error-validation.steps.js
+++ cypress/support/step_definitions/fal6-error-validation.steps.js
@@ -1,78 +1,124 @@
-import {Then} from "@badeball/cypress-cucumber-preprocessor";
+import { Then } from '@badeball/cypress-cucumber-preprocessor';
+import FileUploadPage from '../../e2e/pages/file-upload.page';
 
-const invalidCharacters = [
-  {cellNumber: 'A5', error: 'Enter travel document as P, I, O or as Passport, ID card, Other'},
-  {cellNumber: 'C5', error: 'Enter the country as a 3-letter ISO country code; for example, GBR, SWE, NLD'},
-  {cellNumber: 'D5', error: 'Number of the travel document must be only numbers'},
-  {cellNumber: 'E5', error: 'Enter the surname using English letters instead of special characters not recognised'},
-  {cellNumber: 'F5', error: 'Enter the forenames using English letters instead of special characters not recognised'},
+const invalidCharacters = [{
+  cellNumber: 'A5', error: `Travel document type: Enter travel document as P, I, O or as Passport, ID card, Other`
+}, {
+  cellNumber: 'C5', error: `Travel document country: Travel document country should be a 3-letter ISO country code; for example, GBR, SWE, NLD`
+}, {
+  cellNumber: 'D5', error: `Travel document number: Enter the travel document number using only English letters, numbers or spaces. The following found characters are not allowed: '!'`
+}, {
+  cellNumber: 'E5', error: `Surname: Enter the surname using only English letters, numbers or spaces. The following found characters are not allowed: '*'`
+}, {
+  cellNumber: 'G5', error: `Gender: Enter M for male, F for female, or X for gender neutral if this is in the Travel Document`
+}, {
+  cellNumber: 'I5', error: `Place of birth: Enter the place of birth using only English letters, numbers or spaces. The following found characters are not allowed: '~'`
+}, {
+  cellNumber: 'J5', error: `Nationality: Nationality should be a 3-letter ISO country code; for example, GBR, SWE, NLD`
+}, {
+  cellNumber: 'L5', error: `Cabin: Enter the cabin using only English letters, numbers or spaces. The following found characters are not allowed: '?'`
+},
+//   {
+//   cellNumber: 'M5', error: `Port of embarkation: Enter the port of embarkation using only English letters, numbers, or one of \` -()/\`. The following found characters are not allowed: '{','}'`
+// },
+//   {
+//   cellNumber: 'N5', error: `Port of disembarkation: Enter the port of disembarkation using only English letters, numbers, or one of \` -()/\`. The following found characters are not allowed: '|'`
+// },
   {
-    cellNumber: 'G5',
-    error: 'Enter M for male, F for female, or X for gender neutral if this is in the Travel Document'
-  },
-  {
-    cellNumber: 'I5',
-    error: 'Enter the place of birth using English letters instead of special characters not recognised'
-  },
-  {cellNumber: 'J5', error: 'Enter the country as a 3-letter ISO country code; for example, GBR, SWE, NLD'},
-  {cellNumber: 'L5', error: 'Enter the cabin using only letters and numbers'},
-  {cellNumber: 'M5', error: 'Enter the port of embarkation using only letters and numbers'},
-  {cellNumber: 'N5', error: 'Enter the port of disembarkation using only letters and numbers'},
-  {
-    cellNumber: 'O5',
-    error: 'Enter Yes or Y if this passenger is in transit and No or N if this passenger is disembarking or embarking in the UK'
-  },
-]
+  cellNumber: 'O5', error: `Transit: Enter Yes or Y if this passenger is in transit and No or N if this passenger is disembarking or embarking in the UK`
+},];
 
-const invalidDates = [
-  {cellNumber: 'H5', error: 'Date must be in the dd/mm/yyyy format, for example, 22/02/2002'},
-  {cellNumber: 'K5', error: 'Date must be in the dd/mm/yyyy format, for example, 22/02/2002'},
-]
+const invalidDates = [{
+  cellNumber: 'H5', error: `Date of birth: date of birth must be in the dd/mm/yyyy format, for example, 22/02/2002`
+}, {
+  cellNumber: 'K5', error: `Travel document expiry date: travel document expiry date must be in the dd/mm/yyyy format, for example, 22/02/2002`
+},];
 
-const maxCharacters = [
-  {cellNumber: 'C5', error: 'Enter the country as a 3-letter ISO country code; for example, GBR, SWE, NLD'},
-  {cellNumber: 'D5', error: 'travel document number must be 35 characters or less'},
-  {cellNumber: 'E5', error: 'surname must be 35 characters or less'},
-  {cellNumber: 'F5', error: 'forenames must be 35 characters or less'},
-  {cellNumber: 'G5', error: 'ensure this value has at most 6 characters'},
-  {cellNumber: 'H5', error: 'Date must be in the dd/mm/yyyy format, for example, 22/02/2002'},
-  {cellNumber: 'I5', error: 'place of birth must be 35 characters or less'},
-  {cellNumber: 'J5', error: 'Enter the country as a 3-letter ISO country code; for example, GBR, SWE, NLD'},
-  {cellNumber: 'L5', error: 'cabin must be 35 characters or less'},
-  {cellNumber: 'M5', error: 'port of embarkation must be 35 characters or less'},
-  {cellNumber: 'N5', error: 'port of disembarkation must be 35 characters or less'},
-]
+const maxCharacters = [{
+  cellNumber: 'C5', error: `Travel document country: Travel document country should be a 3-letter ISO country code; for example, GBR, SWE, NLD`
+}, {
+  cellNumber: 'D5', error: `Travel document number: ensure this value has at most 35 characters`
+}, {
+  cellNumber: 'E5', error: `Surname: ensure this value has at most 35 characters`
+}, {
+  cellNumber: 'F5', error: `Forenames: ensure this value has at most 35 characters`
+}, {
+  cellNumber: 'G5', error: `Gender: ensure this value has at most 6 characters`
+}, {
+  cellNumber: 'H5', error: `Date of birth: date of birth must be in the dd/mm/yyyy format, for example, 22/02/2002`
+}, {
+  cellNumber: 'I5', error: `Place of birth: ensure this value has at most 35 characters`
+}, {
+  cellNumber: 'J5', error: `Nationality: Nationality should be a 3-letter ISO country code; for example, GBR, SWE, NLD`
+}, {
+  cellNumber: 'L5', error: `Cabin: ensure this value has at most 35 characters`
+}, {
+  cellNumber: 'M5', error: `Port of embarkation: ensure this value has at most 35 characters`
+}, {
+  cellNumber: 'N5', error: `Port of disembarkation: ensure this value has at most 35 characters`
+},];
 
-const missingOtherDocument = [
-  {
-    cellNumber: 'B5',
-    error: `You entered 'Other' as the travel document type. You must enter the type of travel document, for example SID for Seafarer's Identity Document`
-  },
+const missingOtherDocument = [{
+  cellNumber: 'B5', error: `Travel document nature: You entered 'Other' as the travel document type, so you must enter the nature of the travel document here, for example 'SID' for Seafarer's Identity Document`
+},];
+
+const missingValues = [
+  {cellNumber: 'C5', error: 'Travel document country: field required'},
+  {cellNumber: 'D5', error: 'Travel document number: field required'},
+  {cellNumber: 'E5', error: 'Surname: field required'},
+  {cellNumber: 'F5', error: 'Forenames: field required'},
+  {cellNumber: 'G5', error: 'Gender: field required'},
+  {cellNumber: 'H5',error: 'Date of birth: field required'},
+  {cellNumber: 'M5',error: 'Port of embarkation: field required'},
+  {cellNumber: 'N5',error: 'Port of disembarkation: field required'},
+  {cellNumber: 'O5',error: 'Transit: field required'},
+  {cellNumber: 'A7',error: 'Travel document type: field required'},
 ]
 
 Then('I am shown error messages to fix for {string}', (errorType) => {
-  let errList = []
+  let errList = [];
   switch (errorType) {
-    case 'invalid Characters':
-      errList = invalidCharacters
-      break;
-    case 'invalid Dates':
-      errList = invalidDates
-      break;
-    case 'max Characters':
-      errList = maxCharacters
-      break;
-    case 'missing Other Document':
-      errList = missingOtherDocument
+  case 'invalid Characters':
+    errList = invalidCharacters;
+    break;
+  case 'invalid Dates':
+    errList = invalidDates;
+    break;
+  case 'max Characters':
+    errList = maxCharacters;
+    break;
+  case 'missing Other Document':
+    errList = missingOtherDocument;
+    break;
+    case 'missing values':
+      errList = missingValues;
       break;
   }
-  cy.get('tbody > tr').each(($row) => {
-    const cellNo = $row.find('td').eq(0).text().replace('Cell number', '');
-    const err = $row.find('td').eq(1).text().replace('Error', '').trim();
+  cy.get('tbody > tr')
+    .each(($row) => {
+      const cellNo = $row.find('td')
+        .eq(0)
+        .text()
+        .replace('Cell number', '');
+      const err = $row.find('td')
+        .eq(1)
+        .text()
+        .replace('Error', '')
+        .trim();
 
-    const getErrorForCell = errList.filter(item => item.cellNumber === cellNo.trim());
-    if (getErrorForCell.length > 0) {
-      expect(err).to.be.equal(getErrorForCell[0].error);
-    }
-  });
+      const getErrorForCell = errList.filter(item => item.cellNumber === cellNo.trim());
+      if (getErrorForCell.length > 0) {
+        expect(err)
+          .to
+          .be
+          .equal(getErrorForCell[0].error);
+      }
+    });
+});
+
+Then('I am shown error message for duplicate records for {string}', (fileName) =>{
+if(fileName ==='FAL 5') {
+  FileUploadPage.checkDuplicationRecord(7,8)
+}else
+  FileUploadPage.checkDuplicationRecord(10,11);
 });
