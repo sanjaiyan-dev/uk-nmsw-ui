6e37944a2cd0932e837cb71cd744ab9f2aae7ed4
JenniKate Wallace <jennikate@gmail.com>
2023-10-06 15:46:24 +0100

    Update cypress config for drone

---
+++ cypress/config/drone-staging.config.js
@@ -0,0 +1,15 @@
+const {defineConfig} = require('cypress');
+const defaultConfig = require('../../cypress.config');
+
+module.exports = defineConfig({
+      ...defaultConfig,
+
+      env: {
+        'baseUrl': 'http://localhost:3000',
+        'envName': 'staging',
+        'api_server': 'https://nmsw-api.staging.nmsw.homeoffice.gov.uk/v1',
+        'inboxId': 'b6f7c995-d7b0-48e7-a1b6-9264b9598b37'
+      }
+    }
+);
+
